74c5b42701858e1627f46551608b0fbdf189697d
westofsky <westofsky159@gmail.com>
2023-08-02 12:33:59 +0900

    chore: ESlint, prettier 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -6,9 +6,7 @@ import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-  if (count == 1) {
-    console.log('DSFA');
-  }
+
   return (
     <>
       <div>
@@ -21,12 +19,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
