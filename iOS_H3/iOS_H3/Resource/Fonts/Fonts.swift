be095767598da40936517390fac2f6eeb68680c6
sangyeon <sangyeun33@gmail.com>
2023-08-02 12:50:52 +0900

    design: 사용할 폰트 정의 (#2)

---
+++ iOS_H3/iOS_H3/Resource/Fonts/Fonts.swift
@@ -0,0 +1,36 @@
+//
+//  Fonts.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/02.
+//
+
+import UIKit
+
+enum Fonts {
+    // MARK: - Title
+    static let mediumTitle1 = UIFont(name: "HyundaiSansHeadKRMedium", size: 22)
+    static let mediumTitle2 = UIFont(name: "HyundaiSansHeadKRMedium", size: 20)
+    static let mediumTitle3 = UIFont(name: "HyundaiSansHeadKRMedium", size: 18)
+    static let mediumTitle4 = UIFont(name: "HyundaiSansHeadKRMedium", size: 16)
+    static let mediumTitle5 = UIFont(name: "HyundaiSansHeadKRMedium", size: 14)
+
+    static let regularTitle3 = UIFont(name: "HyundaiSansHeadKRRegular", size: 18)
+    static let regularTitle4 = UIFont(name: "HyundaiSansHeadKRRegular", size: 16)
+    static let regularTitle5 = UIFont(name: "HyundaiSansHeadKRRegular", size: 14)
+    static let regularTitle6 = UIFont(name: "HyundaiSansHeadKRRegular", size: 12)
+
+    // MARK: - Body
+    static let mediumBody1 = UIFont(name: "HyundaiSansTextKROTFMedium", size: 16)
+    static let mediumBody2 = UIFont(name: "HyundaiSansTextKROTFMedium", size: 14)
+    static let mediumBody3 = UIFont(name: "HyundaiSansTextKROTFMedium", size: 12)
+
+    static let regularBody1 = UIFont(name: "HyundaiSansTextKROTFRegular", size: 16)
+    static let regularBody2 = UIFont(name: "HyundaiSansTextKROTFRegular", size: 14)
+    static let regularBody3 = UIFont(name: "HyundaiSansTextKROTFRegular", size: 12)
+
+    // MARK: - Onboarding
+    static let regularOnboardingTitle1 = UIFont(name: "HyundaiSansTextKROTFRegular", size: 26)
+    static let mediumOnboardingTitle2 = UIFont(name: "HyundaiSansTextKROTFMedium", size: 26)
+    static let mediumOnboardingSub = UIFont(name: "HyundaiSansTextKROTFMedium", size: 16)
+}
