a1b511a164359bcc56e7701777928ab939c41c88
westofsky <westofsky159@gmail.com>
2023-08-02 17:27:12 +0900

    style: theme color, reset style 설정 (#4)

---
+++ frontend/src/style/theme.tsx
@@ -0,0 +1,12 @@
+const colors = {
+  Main_Hyundai_Blue: '#0E2B5C',
+  Sub_Active_Blue: '#4CA7CE',
+  Icon_Yellow: '#FFA724',
+  Tag_Sky_Blue: '#DEEBFF',
+  Cool_Grey: '#202732',
+  Cool_Grey_004: '#595C61',
+  Cool_Grey_003: '#A5ACB8',
+  Cool_Grey_002: '#D1D9E3',
+  Cool_Grey_001: '#EDF2FA',
+};
+export {colors};
