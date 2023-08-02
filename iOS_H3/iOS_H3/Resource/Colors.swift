1d586001a3f5ab1bf4f6d052dd4e19b1fce0c496
eigen98 <terrantrace@gmail.com>
2023-08-02 13:38:01 +0900

    Removed file that shouldnt be tracked

---
+++ iOS_H3/iOS_H3/Resource/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/02.
+//
+
+import UIKit
+final class Colors {
+    static let mainHyundaiBlue = UIColor.init(hex: "#0E2B5C")
+    static let subActiveBlue = UIColor.init(hex: "#4CA7CE")
+    static let iconYellow = UIColor.init(hex: "#FFA724")
+    static let tagSkyBlue = UIColor.init(hex: "#DEEBFF")
+    static let coolGreyBlack = UIColor.init(hex: "#202732")
+    static let coolGrey004 = UIColor.init(hex: "#595C61")
+    static let CoolGrey003 = UIColor.init(hex: "#A5ACB8")
+    static let coolGrey002 = UIColor.init(hex: "#D1D9E3")
+    static let coolGrey001 = UIColor.init(hex: "#EDF2FA")
+}
---
+++ iOS_H3/iOS_H3/Resource/UIColor+.swift
@@ -0,0 +1,40 @@
+//
+//  UIColor+.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/02.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    convenience init?(hex: String, alpha: Double=1) {
+        var string = ""
+
+        if hex.lowercased().hasPrefix("0x") {
+            string = hex.replacingOccurrences(of: "0x", with: "")
+        } else if hex.hasPrefix("#") {
+            string = hex.replacingOccurrences(of: "#", with: "")
+        } else {
+            string = hex
+        }
+
+        if string.count == 3 {
+            var str = ""
+            string.forEach {
+                str.append(String(repeating: String($0), count: 2))
+            }
+            string = str
+        }
+
+        guard let hexValue = Int(string, radix: 16) else { return nil }
+
+        var alphaValue = alpha
+        if alpha < 0 { alphaValue = 0 }
+        if alpha > 1 { alphaValue = 1 }
+        let red = CGFloat((hexValue >> 16) & 0xff) / 255
+        let green = CGFloat((hexValue >> 8) & 0xff) / 255
+        let blue = CGFloat(hexValue & 0xff) / 255
+        self.init(red: red, green: green, blue: blue, alpha: CGFloat(alphaValue))
+    }
+}
