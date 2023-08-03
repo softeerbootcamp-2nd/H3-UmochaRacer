87d68e1ef66a063a68b2ed5195776f45898efdc3
eigen98 <terrantrace@gmail.com>
2023-08-03 15:48:01 +0900

    fix : 공통 색상 Colors class에서 enum으로 타입 변경(#1) 1. 정적 멤버만 있기에 인스턴스를 생성할 필요가 없어서 enum으로 변경합니다.

--- iOS_H3/iOS_H3/Resource/Colors.swift
+++ iOS_H3/iOS_H3/Resource/Colors.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
-final class Colors {
+enum Colors {
     static let mainHyundaiBlue = UIColor.init(hex: "#0E2B5C")
     static let subActiveBlue = UIColor.init(hex: "#4CA7CE")
     static let iconYellow = UIColor.init(hex: "#FFA724")
     static let tagSkyBlue = UIColor.init(hex: "#DEEBFF")
     static let coolGreyBlack = UIColor.init(hex: "#202732")
-    static let coolGrey004 = UIColor.init(hex: "#595C61")
-    static let CoolGrey003 = UIColor.init(hex: "#A5ACB8")
-    static let coolGrey002 = UIColor.init(hex: "#D1D9E3")
-    static let coolGrey001 = UIColor.init(hex: "#EDF2FA")
+    static let coolGrey4 = UIColor.init(hex: "#595C61")
+    static let coolGrey3 = UIColor.init(hex: "#A5ACB8")
+    static let coolGrey2 = UIColor.init(hex: "#D1D9E3")
+    static let coolGrey1 = UIColor.init(hex: "#EDF2FA")
 }
