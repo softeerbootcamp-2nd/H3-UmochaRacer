c31855fb91186a95a78b63af881f191fec0a149c
sangyeon <sangyeun33@gmail.com>
2023-08-03 15:33:56 +0900

    feat: UILabel의 line height 설정해주는 메소드 구현 (#2)
    font에는 line height를 설정해줄 수 없어서 UILabel에 attributedString을 이용하여 line height를 적용해주는 메소드를 구현했습니다.
    자세한 내용은 https://www.notion.so/Font-line-height-letter-spacing-4a9b5cb3a0934143a043987512d3a507?pvs=4 참고

---
+++ iOS_H3/iOS_H3/Resource/UILabel+.swift
@@ -0,0 +1,29 @@
+//
+//  UILabel+.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/03.
+//
+
+import UIKit
+
+extension UILabel {
+    func setupLineHeight(_ lineHeight: CGFloat) {
+        guard let text else { return }
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.minimumLineHeight = lineHeight
+        paragraphStyle.maximumLineHeight = lineHeight
+
+        let attributes: [NSAttributedString.Key: Any] = [
+            .paragraphStyle: paragraphStyle,
+            .baselineOffset: (lineHeight - font.lineHeight) / 2
+        ]
+        let attributedString = NSAttributedString(
+            string: text,
+            attributes: attributes
+        )
+
+        attributedText = attributedString
+    }
+}
