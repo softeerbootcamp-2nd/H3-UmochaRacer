046ccb373f9b37369e829413947087369f1fde23
KimJungHun <j7papa@naver.com>
2023-08-06 00:34:33 +0900

    feat: 자세히보기 아코디언 애니메이션 구현 (#20)

--- frontend/src/component/content/right/card/OptionCard.tsx
+++ frontend/src/component/content/right/card/OptionCard.tsx
@@ -1,41 +1,62 @@
 import React from 'react';
+import {useState, useRef, useEffect, useCallback} from 'react';
 import styled from 'styled-components';
 import {colors} from '@/style/theme';
+import DetailToggle from './DetailToggle';
+
+const Icon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM10.643 16.243L17.713 9.172L16.299 7.758L10.643 13.415L7.814 10.586L6.4 12L10.643 16.243Z"
+        fill="#0E2B5C"
+      />
+    </svg>
+  );
+};
+
+let detailHeight = 0;
 
 function OptionCard() {
+  const [toggle, setToggle] = useState(false); // 클릭 여부 상태 관리
+  const contentBoxRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const clickedToggle = useCallback(
+    (event: React.MouseEvent) => {
+      event.stopPropagation();
+      if (contentBoxRef.current === null || contentRef.current === null) {
+        return;
+      }
+
+      if (contentBoxRef.current.clientHeight > 0) {
+        contentBoxRef.current.style.height = '0';
+      } else {
+        contentBoxRef.current.style.height = `${contentRef.current.clientHeight}px`;
+      }
+
+      setToggle(!toggle);
+    },
+    [toggle],
+  );
+
   return (
     <Wrapper>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-      >
-        <path
-          d="M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM10.643 16.243L17.713 9.172L16.299 7.758L10.643 13.415L7.814 10.586L6.4 12L10.643 16.243Z"
-          fill="#0E2B5C"
-        />
-      </svg>
+      {Icon()}
       <Text1>구매자의 63%가 선택했어요!</Text1>
       <Text2>디젤 2.2</Text2>
+      <DetailBox ref={contentBoxRef} toggle={toggle}>
+        <DetailContent ref={contentRef}>컨텐츠</DetailContent>
+      </DetailBox>
       <Footer>
         <Price>+ 1,480,000원</Price>
-        <Detail>
-          자세히 보기
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 14 14"
-            fill="none"
-          >
-            <path
-              d="M7.77818 7.84802L12.0129 3.88813L13.2226 5.0193L7.77818 10.1104L2.33374 5.0193L3.54343 3.88813L7.77818 7.84802Z"
-              fill="#AEB1B7"
-            />
-          </svg>
-        </Detail>
+        <DetailToggle onClick={clickedToggle} isOpen={toggle}></DetailToggle>
       </Footer>
     </Wrapper>
   );
@@ -48,7 +69,7 @@ const Wrapper = styled.div`
   flex-shrink: 0;
   flex-direction: column;
   width: 375px;
-  height: 150px;
+  min-height: 150px;
   padding: 20px;
   border-radius: 6px;
   border: 2px solid ${colors.Main_Hyundai_Blue};
@@ -86,15 +107,17 @@ const Price = styled.div`
   color: ${colors.Main_Hyundai_Blue};
 `;
 
-const Detail = styled.div`
-  display: inline-flex;
-  align-items: center;
-  height: 16px;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 130%; /* 15.6px */
-  letter-spacing: -0.36px;
-  color: ${colors.Cool_Grey_003};
-  gap: 2px;
+const DetailBox = styled.div<{toggle: boolean}>`
+  height: 0;
+  overflow: hidden;
+  opacity: ${(props) => (props.toggle ? '1' : '0')};
+  border-top: 1px solid ${colors.Cool_Grey_001};
+  transition:
+    height 0.5s,
+    opacity 0.5s;
+`;
+
+const DetailContent = styled.div`
+  padding-top: 10px;
+  padding-bottom: 10px;
 `;
