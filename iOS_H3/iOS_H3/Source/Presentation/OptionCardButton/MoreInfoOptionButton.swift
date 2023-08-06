6597e60a3fc409293d38d01e7fbbfe9406305946
eigen98 <terrantrace@gmail.com>
2023-08-06 21:46:04 +0900

    feat : MoreInfoOptionButton 클래스 구현 _ 자세히보기 옵션 (#21) 1. 기존 OptionCardButton 클래스를 확장하여 상세 정보를 확인할 수 있는 기능을 추가하였습니다. 2. 사용자가 간편하게 추가 정보를 접근할 수 있도록 화살표 버튼을 포함하여 구현하였습니다.

---
+++ iOS_H3/iOS_H3/Source/Presentation/OptionCardButton/MoreInfoOptionButton.swift
@@ -0,0 +1,45 @@
+//
+//  MoreInfoOptionButton.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/06.
+//
+
+import Foundation
+import UIKit
+// 자세히보기 옵션 버튼
+class MoreInfoOptionButton: OptionCardButton {
+    private let moreInfoButton: UIButton = {
+        let button = UIButton()
+
+        button.setImage(UIImage(named: "arrow_right_img"), for: .normal)
+        return button
+    }()
+
+    override init(type: OptionCardButton.OptionCardType,
+                  optionTitle: String = "옵션 타이틀",
+                  optionSubTitle: String = "옵션 서브 타이틀",
+                  price: String = "+ 0원") {
+        super.init(type: type)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func attribute() {
+    }
+
+    private func layout() {
+        self.addSubview(moreInfoButton)
+        moreInfoButton.translatesAutoresizingMaskIntoConstraints = false
+
+        moreInfoButton.widthAnchor.constraint(equalToConstant: 18).isActive = true
+        moreInfoButton.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        moreInfoButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -18).isActive = true
+        moreInfoButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+
+    }
+
+}
