7345b553b8244f319018ddc6cad488dc186245a5
sangyeon <sangyeun33@gmail.com>
2023-08-08 01:07:51 +0900

    feat: TwoOptionCardButtonView 구현 (#23)
    - 2개의 옵션 카드 버튼을 가진 TwoOptionCardButtonView 구현
      - 인덱스에 해당하는 버튼을 선택하고 나머지 버튼을 선택 해제해주는 selectOption(index:) 메소드 구현
      - index에 해당하는 옵션 카드 버튼의 view를 업데이트 해주는 updateView(index:,with:) 메소드 구현
      - 카드 info에 따라 모든 옵션 카드 버튼의 view를 업데이트 해주는 updateAllViews(with:) 메소드 구현

---
+++ iOS_H3/iOS_H3/Source/Presentation/TwoOptionCardButtonView.swift
@@ -0,0 +1,122 @@
+//
+//  TwoOptionCardButtonView.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/07.
+//
+
+import UIKit
+
+final class TwoOptionCardButtonView: UIView {
+
+    enum Constants {
+        static let buttonSpacing = 12.0
+    }
+
+    // MARK: - UI Properties
+
+    private let optionCardButtons: [OptionCardButton]
+
+    // MARK: - Properties
+
+    private var selectedButtonIndex = 0
+
+    // MARK: - LifeCycles
+
+    init(frame: CGRect = .zero, type: OptionCardButton.OptionCardType) {
+        optionCardButtons = (0..<2).map { _ in OptionCardButton(type: type) }
+        super.init(frame: frame)
+
+        setupOptionCardButtons()
+        setupViews()
+    }
+
+    override init(frame: CGRect) {
+        optionCardButtons = (0..<2).map { _ in OptionCardButton(type: .selfMode) }
+        super.init(frame: frame)
+
+        setupOptionCardButtons()
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        optionCardButtons = (0..<2).map { _ in OptionCardButton(type: .selfMode) }
+        super.init(coder: coder)
+
+        setupOptionCardButtons()
+        setupViews()
+    }
+
+    // MARK: - Helpers
+
+    func selectOption(index: Int) {
+        optionCardButtons[selectedButtonIndex].isSelected = false
+        optionCardButtons[index].isSelected = true
+        selectedButtonIndex = index
+    }
+
+    /// index에 해당하는 옵션 카드의 view를 업데이트
+    func updateView(index: Int, with cardInfo: OptionCardInfo) {
+        optionCardButtons[index].setOptionTitle(cardInfo.title)
+        optionCardButtons[index].setOptionSubTitle(cardInfo.subTitle)
+        optionCardButtons[index].setPrice(cardInfo.priceString)
+    }
+
+    /// 카드 info에 따라 모든 옵션 카드의 view를 업데이트
+    func updateAllViews(with cardInfos: [OptionCardInfo]) {
+        optionCardButtons.enumerated().forEach { (index, _) in
+            updateView(index: index, with: cardInfos[index])
+        }
+    }
+}
+
+extension TwoOptionCardButtonView {
+
+    private func setupOptionCardButtons() {
+        optionCardButtons.forEach { button in
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.addTarget(self, action: #selector(optionCardButtonDidTapped(_:)), for: .touchUpInside)
+        }
+    }
+
+    private func setupViews() {
+        addSubviews()
+        setupConstraints()
+        selectOption(index: selectedButtonIndex)
+    }
+
+    @objc
+    private func optionCardButtonDidTapped(_ sender: UIButton) {
+        guard let selectedOptionIndex = optionCardButtons.firstIndex(where: { $0 == sender }) else {
+            return
+        }
+        selectOption(index: selectedOptionIndex)
+    }
+
+    private func addSubviews() {
+        optionCardButtons.forEach {
+            addSubview($0)
+        }
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            optionCardButtons[0].topAnchor.constraint(equalTo: self.topAnchor),
+            optionCardButtons[0].leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            optionCardButtons[0].trailingAnchor.constraint(
+                equalTo: self.centerXAnchor,
+                constant: -(Constants.buttonSpacing / 2)
+            ),
+            optionCardButtons[0].bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            optionCardButtons[1].topAnchor.constraint(equalTo: optionCardButtons[0].topAnchor),
+            optionCardButtons[1].leadingAnchor.constraint(
+                equalTo: self.centerXAnchor,
+                constant: (Constants.buttonSpacing / 2)
+            ),
+            optionCardButtons[1].trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            optionCardButtons[1].bottomAnchor.constraint(equalTo: self.optionCardButtons[0].bottomAnchor)
+        ])
+    }
+}
