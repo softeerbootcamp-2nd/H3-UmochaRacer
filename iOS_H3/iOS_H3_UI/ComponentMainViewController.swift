a100d6b4708e7287bf2393e1e69f448584fca0de
eigen98 <terrantrace@gmail.com>
2023-08-08 09:25:04 +0900

    chore : UI컴포넌트 target membership 업데이트 및 위치 변경_ iOS_H3 -> iOS_H3_UI (#64) 1. TitleBar, 공통 버튼, 옵션 버튼 컴포넌트들을 UI 타겟으로 이동하였습니다. 2. 이 변경을 통해 프로젝트의 구조를 더욱 명확하게 하였으며, 코드 재사용성을 높이고 유지보수를 용이하게 하였습니다.

--- iOS_H3/iOS_H3_UI/ComponentMainViewController.swift
+++ iOS_H3/iOS_H3_UI/ComponentMainViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ComponentMainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
     }
 
 }
