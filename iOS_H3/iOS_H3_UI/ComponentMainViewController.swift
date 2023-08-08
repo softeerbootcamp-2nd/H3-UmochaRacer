027274176fe54ccd33a53294b9bacdb150036de5
eigen98 <terrantrace@gmail.com>
2023-08-08 09:30:59 +0900

    feat : UI 컴포넌트를 보여주는 메인 뷰 컨트롤러 추가(#64) 1. ComponentMainViewController 클래스를 생성하여 UI 컴포넌트들을 시각적으로 확인할 수 있는 메인 뷰 컨트롤러를 구현하였습니다. 2. 스크롤 뷰와 스택 뷰를 사용하여 네비게이션바, 옵션카드버튼, 공통버튼을 표시하는 버튼을 배치하였습니다.

--- iOS_H3/iOS_H3_UI/ComponentMainViewController.swift
+++ iOS_H3/iOS_H3_UI/ComponentMainViewController.swift
@@ -9,10 +9,103 @@ import UIKit
 
 class ComponentMainViewController: UIViewController {
 
+    lazy private var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    lazy private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 18
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    lazy private var titleBarButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("네비게이션바 보기", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(didTapTitleBarButton), for: .touchUpInside)
+        return button
+    }()
+
+    lazy private var optionCardButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("옵션카드버튼 보기", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(didTapOptionCardButton), for: .touchUpInside)
+        return button
+    }()
+
+    lazy private var ohMyCarSetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("공통버튼 보기", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(didTapButtons), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.view.backgroundColor = .white
+        setupViews()
+
+    }
+
+    func setupViews() {
+        setupContainerView()
+        setupUIComponentButtons()
+
+    }
+
+    func setupContainerView() {
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,
+                                            constant: 20),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+        ])
+    }
+
+    func setupUIComponentButtons() {
+        stackView.addArrangedSubview(titleBarButton)
+        stackView.addArrangedSubview(optionCardButton)
+        stackView.addArrangedSubview(ohMyCarSetButton)
+
+        let buttons = [titleBarButton, optionCardButton, ohMyCarSetButton]
+        for button in buttons {
+            button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        }
+    }
+
+    @objc func didTapTitleBarButton() {
+        let titleBarVC = OhMyCarSetTitleBarViewController()
+        self.present(titleBarVC, animated: false)
+    }
+
+    @objc func didTapOptionCardButton() {
+        let titleBarVC = OptionCardButtonViewController()
+        self.present(titleBarVC, animated: false)
+    }
+
+    @objc func didTapButtons() {
+        let titleBarVC = OhMyCarSetButtonViewController()
+        self.present(titleBarVC, animated: false)
     }
 
 }
