89877c293f73e53b67e9e559c6ee6e983e44f169
westofsky <westofsky159@gmail.com>
2023-08-09 14:05:56 +0900

    feat: 페이지 정렬 (#60)

--- frontend/src/component/main/MainContainer.tsx
+++ frontend/src/component/main/MainContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from '../header/Header';
 import styled from 'styled-components';
 import main from '@/assets/images/main.png';
@@ -8,26 +8,36 @@ import TrimCardList from './intro/trimCard/TrimCard';
 import IntroTitle from './intro/IntroTitle';
 import IntroShowMore from './intro/IntroShowMore';
 function MainContainer() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', updateScroll);
+  });
   return (
     <>
-      <Main.Intro>
-        <Main.Header>
+      <Intro.Wrapper>
+        <Intro.Header className={scrollPosition < 1 ? '' : 'change_header'}>
           <Header />
-        </Main.Header>
-        <Main.Content>
+        </Intro.Header>
+        <Intro.Content>
           <IntroTitle />
           <TrimCardList />
           <IntroShowMore />
-        </Main.Content>
-      </Main.Intro>
-      asdfl;fsf
+        </Intro.Content>
+      </Intro.Wrapper>
+      <Trim.Header>
+        <Trim.IntroP>모델 한 눈에 비교하기</Trim.IntroP>
+        <Trim.ModelWrapper></Trim.ModelWrapper>
+      </Trim.Header>
     </>
   );
 }
 
 export default MainContainer;
-const Main = {
-  Intro: styled.div`
+const Intro = {
+  Wrapper: styled.div`
     height: 100vh;
     ${flexCenter}
     width: 100vw;
@@ -41,6 +51,10 @@ const Main = {
     left: 0;
     z-index: 100;
     width: 100%;
+    transition: 0.2s ease-in-out;
+    &.change_header {
+      background: #e7e7e7;
+    }
   `,
   Content: styled.div`
     width: 1024px;
@@ -48,5 +62,28 @@ const Main = {
     padding-top: 85px;
     display: flex;
     flex-direction: column;
+  `,
+};
+
+const Trim = {
+  Header: styled.div`
+    width: 100%;
+    height: 335px;
+    border-bottom: 1px solid #bebebe;
+    background: #e7e7e7;
+  `,
+  IntroP: styled.p`
+    color: #212121;
+    font-family: Hyundai Sans Head KR;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 130%;
+    letter-spacing: -1.6px;
+    text-align: center;
+    padding-top: 100px;
+  `,
+  ModelWrapper: styled.div`
+    width: 1024px;
   `,
 };
