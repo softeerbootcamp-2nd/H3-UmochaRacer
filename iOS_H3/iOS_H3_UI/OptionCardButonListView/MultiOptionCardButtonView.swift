698abc109778485f61d6f80a4ea97c48ae534b20
sangyeon <sangyeun33@gmail.com>
2023-08-09 16:38:58 +0900

    feat: MultiOptionCardButtonView - optionCardCollectionView 구현 (#24)
    - DiffableDataSource와 CompositionalLayout 이용
    - 가로 스크롤과 페이지 단위 스크롤 적용
    - updateView(with:) 메소드 호출하여 collectionView 업데이트 가능

---
+++ iOS_H3/iOS_H3_UI/OptionCardButonListView/MultiOptionCardButtonView.swift
@@ -0,0 +1,147 @@
+//
+//  MultiOptionCardButtonView.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/09.
+//
+
+import UIKit
+
+final class MultiOptionCardButtonView: UIView {
+    
+    enum Constants {
+        static let cellHeight = 150.0
+        static let optionCardCollectionViewHeight = 150.0
+    }
+    
+    enum Section {
+        case optionCard
+    }
+    
+    typealias OptionCardCollectionViewDiffableDataSource = UICollectionViewDiffableDataSource<Section, OptionCardInfo>
+    
+    // MARK: - UI properties
+    
+    private var optionCardCollectionView: UICollectionView!
+    
+    // MARK: - Properties
+    
+    private let optionCardType: OptionCardButton.OptionCardType
+
+    private var dataSource: OptionCardCollectionViewDiffableDataSource!
+
+    private let cellIdentifiers: [OptionCardButton.OptionCardType: String] = [
+        .selfMode: SelfModeOptionCardCell.identifier,
+        .guideMode: GuideModeOptionCardCell.identifier
+    ]
+    
+    // MARK: - Lifecycles
+    
+    init(frame: CGRect = .zero, type: OptionCardButton.OptionCardType) {
+        optionCardType = type
+        super.init(frame: frame)
+        
+        setupOptionCardCollectionView()
+        setupViews()
+    }
+    
+    override init(frame: CGRect) {
+        optionCardType = .selfMode
+        super.init(frame: frame)
+        
+        setupOptionCardCollectionView()
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        optionCardType = .selfMode
+        super.init(coder: coder)
+        
+        setupOptionCardCollectionView()
+        setupViews()
+    }
+    
+    // MARK: - Helpers
+    
+    func updateView(with cardInfos: [OptionCardInfo]) {
+        var snapshot = dataSource.snapshot()
+        
+        let previousItem = snapshot.itemIdentifiers
+        snapshot.deleteItems(previousItem)
+        snapshot.appendItems(cardInfos)
+        
+        dataSource.apply(snapshot)
+    }
+}
+
+// MARK: - Setup
+
+extension MultiOptionCardButtonView {
+
+    private func setupOptionCardCollectionView() {
+        optionCardCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createCollectionViewLayout())
+        optionCardCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        optionCardCollectionView.isScrollEnabled = true
+        optionCardCollectionView.bounces = false
+        
+        registerCollectionViewCell()
+        setupCollectionViewDataSource()
+        setupSnapshot()
+    }
+
+    private func setupViews() {
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func createCollectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(Constants.cellHeight))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(Constants.cellHeight))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func registerCollectionViewCell() {
+        optionCardCollectionView.register(SelfModeOptionCardCell.self, forCellWithReuseIdentifier: SelfModeOptionCardCell.identifier)
+        optionCardCollectionView.register(GuideModeOptionCardCell.self, forCellWithReuseIdentifier: GuideModeOptionCardCell.identifier)
+    }
+    
+    private func setupCollectionViewDataSource() {
+        dataSource = OptionCardCollectionViewDiffableDataSource(collectionView: optionCardCollectionView) { [weak self] (collectionView, indexPath, item) in
+            guard let self,
+                  let cellIdentifier = cellIdentifiers[optionCardType],
+                  let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? OptionCardCell else {
+                return OptionCardCell()
+            }
+            
+            cell.configure(item)
+            
+            return cell
+        }
+    }
+    
+    private func setupSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, OptionCardInfo>()
+        snapshot.appendSections([Section.optionCard])
+        dataSource.apply(snapshot)
+    }
+
+    private func addSubviews() {
+        addSubview(optionCardCollectionView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            optionCardCollectionView.topAnchor.constraint(equalTo: self.topAnchor),
+            optionCardCollectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            optionCardCollectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            optionCardCollectionView.heightAnchor.constraint(equalToConstant: Constants.optionCardCollectionViewHeight)
+        ])
+    }
+}
