0a1e4166a9a16a72ca960838fbba015e28ebd806
Park Seonmi <sm000117@naver.com>
2023-08-09 15:56:07 +0900

    feat: 내장색상 정보 반환하는 API (#69)
    외장색상에 종속되기 때문에 파라미터로 값을 받음

--- backend/src/main/java/com/example/backend/domain/information/model/car/repository/InteriorColorRepository.java
+++ backend/src/main/java/com/example/backend/domain/information/model/car/repository/InteriorColorRepository.java
@@ -3,13 +3,12 @@ package com.example.backend.domain.information.model.car.repository;
 import com.example.backend.domain.information.model.car.entity.InteriorColor;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface InteriorColorRepository extends CrudRepository<InteriorColor, Long> {
-    @Query("SELECT ic FROM InteriorColor ic WHERE ic.exteriorColorId := id")
-    List<InteriorColor> findAllByExteriorColorId(@Param("id")long exteriorColorId);
+    @Query("SELECT * FROM Interior_Color ic WHERE ic.exterior_color_id = :exteriorColorId OR ic.exterior_color_id IS NULL")
+    List<InteriorColor> findAllByExteriorColorId(long exteriorColorId);
 }
