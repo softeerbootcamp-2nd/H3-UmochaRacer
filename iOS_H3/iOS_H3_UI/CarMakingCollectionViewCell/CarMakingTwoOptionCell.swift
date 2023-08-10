3e5481d8a61ec4fe16e02b36ffc87da098aa2dd3
eigen98 <terrantrace@gmail.com>
2023-08-10 09:14:26 +0900

    feat : CarMakingTwoOptionCell 클래스 구현 (#26) 1. CarMakingCollectionViewCell을 상속받아 두 개의 옵션을 가지는 셀을 구현 2. 특정 시나리오에서 두 가지 선택지만 필요할 때 사용되며, 기본 CarMakingCollectionViewCell의 확장으로 구현

--- iOS_H3/iOS_H3_UI/CarMakingCollectionViewCell/CarMakingTwoOptionCell.swift
+++ iOS_H3/iOS_H3_UI/CarMakingCollectionViewCell/CarMakingTwoOptionCell.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+class CarMakingTwoOptionCell: CarMakingCollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override init(frame: CGRect = .zero, buttonListViewable: OptionCardButtonListViewable) {
+        super.init(frame: frame, buttonListViewable: buttonListViewable)
+    }
+
+}
