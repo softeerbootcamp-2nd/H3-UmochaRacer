e5ad693e04b6691ae36e33212b95450ee4cb04d0
Park Seonmi <sm000117@naver.com>
2023-08-10 21:15:25 +0900

    fix: TrimExteriorRepository 쿼리 수정 (#85)
    왜 언더바 없지..

--- backend/src/main/java/com/example/backend/domain/information/model/car/repository/TrimExteriorRepository.java
+++ backend/src/main/java/com/example/backend/domain/information/model/car/repository/TrimExteriorRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface TrimExteriorRepository extends CrudRepository<TrimExterior, Long> {
     @Query(value = "SELECT te.id, te.trim_id, te.exterior_color_id,\n" +
-            "       t.name AS trimname, ex.name AS exname, ex.color_code\n" +
+            "       t.name AS trim_name, ex.name AS ex_name, ex.color_code\n" +
             "FROM Trim_Exterior te \n" +
             "LEFT OUTER JOIN Trim t ON t.id = te.trim_id \n" +
             "LEFT OUTER JOIN Exterior_color ex ON ex.id = te.exterior_color_id",
