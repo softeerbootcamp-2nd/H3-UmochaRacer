d75d397b9effe1ffda5d2b880d62fad64f853dbb
sangyeon <sangyeun33@gmail.com>
2023-08-12 14:20:44 +0900

    chore: OptionCardCell - swiftLint 맞춤

--- iOS_H3/iOS_H3_UI/OptionCardButonListView/OptionCardCell.swift
+++ iOS_H3/iOS_H3_UI/OptionCardButonListView/OptionCardCell.swift
@@ -9,7 +9,7 @@ import UIKit
 import Combine
 
 class OptionCardCell: UICollectionViewCell {
-    
+
     // MARK: - UI properties
 
     private let optionCardButton: OptionCardButton
@@ -27,7 +27,7 @@ class OptionCardCell: UICollectionViewCell {
         setupViews()
         setupButtonTapSubject()
     }
-    
+
     init?(coder: NSCoder, type: OptionCardButton.OptionCardType) {
         optionCardButton = OptionCardButton(type: type)
         super.init(coder: coder)
@@ -51,7 +51,7 @@ class OptionCardCell: UICollectionViewCell {
         setupViews()
         setupButtonTapSubject()
     }
-    
+
     override func prepareForReuse() {
         super.prepareForReuse()
         optionCardButton.isSelected = false
@@ -78,11 +78,11 @@ extension OptionCardCell {
             optionCardButton.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
-    
+
     private func setupButtonTapSubject() {
         optionCardButton.addTarget(self, action: #selector(optionButtonDidTapped), for: .touchUpInside)
     }
-    
+
     @objc
     private func optionButtonDidTapped() {
         buttonTapSubject.send(())
