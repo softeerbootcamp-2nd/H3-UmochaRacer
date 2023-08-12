9c998cb941514080ecc88f042e32c8593b8dd253
sangyeon <sangyeun33@gmail.com>
2023-08-12 17:04:09 +0900

    design: BottomModalViewController 구현 (#93)

---
+++ iOS_H3/iOS_H3_UI/BottomModalViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BottomModalViewController.swift
+//  iOS_H3_UI
+//
+//  Created by  sangyeon on 2023/08/12.
+//
+
+import UIKit
+
+final class BottomModalViewController: UIViewController {
+
+    enum Constants {
+        static let bottomModalViewHeight = 129.0
+    }
+
+    // MARK: - UI properties
+
+    private let bottomModalView = BottomModalView()
+
+    // MARK: - Lifecycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+    }
+
+    // MARK: - Helpers
+}
+
+extension BottomModalViewController {
+
+    private func setupViews() {
+        view.backgroundColor = .systemBlue
+        bottomModalView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(bottomModalView)
+        NSLayoutConstraint.activate([
+            bottomModalView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomModalView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomModalView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomModalView.heightAnchor.constraint(equalToConstant: Constants.bottomModalViewHeight)
+        ])
+    }
+}
