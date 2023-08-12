1aab76b45aa5bc6e737d0dda1affcbb29af91261
eigen98 <terrantrace@gmail.com>
2023-08-12 15:27:00 +0900

    feat : 이미지 캐시 매니저 클래스 추가(#25) 1. NSCache를 활용한 이미지 캐시 매니저 클래스를 구현하였습니다. 2. `setObject` 메소드를 통해 이미지를 캐시에 저장 3. `object` 메소드를 통해 특정 키에 해당하는 이미지를 캐시로부터 가져올 수 있습니다.

--- iOS_H3/iOS_H3/Source/ImageCacheManager.swift
+++ iOS_H3/iOS_H3/Source/ImageCacheManager.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+final class ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+
+    func setObject(_ image: UIImage, forKey key: NSString) {
+        ImageCacheManager.shared.setObject(image, forKey: key)
+    }
+
+    func object(forKey key: NSString) -> UIImage? {
+        return ImageCacheManager.shared.object(forKey: key)
+    }
+}
