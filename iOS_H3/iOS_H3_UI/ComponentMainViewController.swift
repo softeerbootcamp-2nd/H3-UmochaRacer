45fbd93b069fe0b667c57c93e50e6614d5a7a571
eigen98 <terrantrace@gmail.com>
2023-08-13 17:48:52 +0900

    feat : 컴포넌트 메인 뷰컨트롤러에서 OptionCategoryTabbar 뷰컨트롤러로 화면전환 반영(#99)

--- iOS_H3/iOS_H3_UI/ComponentMainViewController.swift
+++ iOS_H3/iOS_H3_UI/ComponentMainViewController.swift
@@ -63,6 +63,14 @@ class ComponentMainViewController: UIViewController {
         return button
     }()
 
+    lazy private var optionCategoryTabbarButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("OptionCategoryTabbar 보기", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(didTapOptionCategoryTabbarButton), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -101,13 +109,15 @@ class ComponentMainViewController: UIViewController {
         stackView.addArrangedSubview(ohMyCarSetButton)
         stackView.addArrangedSubview(progressBarButton)
         stackView.addArrangedSubview(twoOptionCardButtonViewButton)
+        stackView.addArrangedSubview(optionCategoryTabbarButton)
 
         let buttons = [
             titleBarButton,
             optionCardButton,
             ohMyCarSetButton,
             progressBarButton,
-            twoOptionCardButtonViewButton
+            twoOptionCardButtonViewButton,
+            optionCategoryTabbarButton
         ]
         for button in buttons {
             button.heightAnchor.constraint(equalToConstant: 30).isActive = true
@@ -136,6 +146,11 @@ class ComponentMainViewController: UIViewController {
 
     @objc func didTapTwoOptionCardButtonViewButton() {
         let viewController = TwoOptionCardButtonViewController()
+        self.present(viewController, animated: false)
+    }
+
+    @objc func didTapOptionCategoryTabbarButton() {
+        let viewController = OptionCategoryTabbarViewController()
         self.present(viewController, animated: false)
     }
 }
