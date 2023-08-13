543a188164e793d43300e3e1efa42b334fb5e803
eigen98 <terrantrace@gmail.com>
2023-08-14 08:59:05 +0900

    fix : 이미지 로딩 함수의 파라미터 이름 변경 및 로딩 인디케이터 로직 개선(#25) 1. `loadCachedImage` 함수의 `showLoadingIndicator` 파라미터 이름을 `showLoading`으로 변경 2. 로딩 인디케이터를 보여주는 로직을 개선

--- iOS_H3/iOS_H3/Source/Extensions/UIImageView+.swift
+++ iOS_H3/iOS_H3/Source/Extensions/UIImageView+.swift
@@ -9,45 +9,48 @@
 import UIKit
 
 extension UIImageView {
-    func loadCachedImage(of key: String, showLoadingIndicator: Bool = true) {
-
+    private var activityIndicator: UIActivityIndicatorView? {
+        return self.subviews.first { $0 is UIActivityIndicatorView } as? UIActivityIndicatorView
+    }
+    
+    private func showLoadingIndicator() {
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        self.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
+    
+    private func hideLoadingIndicator() {
+        activityIndicator?.removeFromSuperview()
+    }
+    
+    func loadCachedImage(of key: String, showLoading: Bool = true) {
         if let cachedImage = ImageCacheManager.shared.object(forKey: key as NSString) {
             self.image = cachedImage
             return
         }
 
-        if showLoadingIndicator {
-            let activityIndicator = UIActivityIndicatorView(style: .medium)
-            activityIndicator.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
-            self.addSubview(activityIndicator)
-            activityIndicator.startAnimating()
+        if showLoading {
+            showLoadingIndicator()
         }
 
-        DispatchQueue.global().async {
-            guard
-                let url = URL(string: key),
-                var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
-            else { return }
+        guard let url = URL(string: key),
+              var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        else { return }
 
-            urlComponents.scheme = "https"
-            guard let imageURL = urlComponents.url else { return }
+        urlComponents.scheme = "https"
+        guard let imageURL = urlComponents.url else { return }
 
-            URLSession.shared.dataTask(with: imageURL) { [weak self] data, response, error in
-                guard
-                    let data = data,
-                    let loadedImage = UIImage(data: data),
-                    error == nil
-                else { return }
-                
-                ImageCacheManager.shared.setObject(loadedImage, forKey: key as NSString)
-                
-                DispatchQueue.main.async {
-                    if showLoadingIndicator {
-                        self?.subviews.forEach { $0.removeFromSuperview() }
-                    }
-                    self?.image = loadedImage
-                }
-            }.resume()
+        let task = URLSession.shared.dataTask(with: imageURL) { [weak self] data, response, error in
+            guard let data = data, let loadedImage = UIImage(data: data), error == nil else { return }
+            
+            ImageCacheManager.shared.setObject(loadedImage, forKey: key as NSString)
+            
+            DispatchQueue.main.async {
+                self?.hideLoadingIndicator()
+                self?.image = loadedImage
+            }
         }
+        task.resume()
     }
 }
