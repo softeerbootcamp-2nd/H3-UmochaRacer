795a8ffd2b54417ea181f1b3c3696f0ef4b9f6ed
eigen98 <terrantrace@gmail.com>
2023-08-14 09:07:34 +0900

    feat : 로딩 인디케이터 로직을 UIView의 Extension으로 분리(#25) 1.  UIView에 로딩 인디케이터를 보여주고 숨기는 메서드를 추가 2. 이미지 로딩 시 인디케이터를 보여주는 로직을 UIImageView의 Extension에서 UIView의 Extension으로 이동

--- iOS_H3/iOS_H3/Source/Extensions/UIImageView+.swift
+++ iOS_H3/iOS_H3/Source/Extensions/UIImageView+.swift
@@ -5,25 +5,9 @@
 //  Created by KoJeongMin  on 2023/08/12.
 //
 
-
 import UIKit
 
 extension UIImageView {
-    private var activityIndicator: UIActivityIndicatorView? {
-        return self.subviews.first { $0 is UIActivityIndicatorView } as? UIActivityIndicatorView
-    }
-    
-    private func showLoadingIndicator() {
-        let activityIndicator = UIActivityIndicatorView(style: .medium)
-        activityIndicator.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
-        self.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-    }
-    
-    private func hideLoadingIndicator() {
-        activityIndicator?.removeFromSuperview()
-    }
-    
     func loadCachedImage(of key: String, showLoading: Bool = true) {
         if let cachedImage = ImageCacheManager.shared.object(forKey: key as NSString) {
             self.image = cachedImage
@@ -43,9 +27,7 @@ extension UIImageView {
 
         let task = URLSession.shared.dataTask(with: imageURL) { [weak self] data, response, error in
             guard let data = data, let loadedImage = UIImage(data: data), error == nil else { return }
-            
             ImageCacheManager.shared.setObject(loadedImage, forKey: key as NSString)
-            
             DispatchQueue.main.async {
                 self?.hideLoadingIndicator()
                 self?.image = loadedImage
---
+++ iOS_H3/iOS_H3/Source/Extensions/UIView+.swift
@@ -0,0 +1,25 @@
+//
+//  UIView+.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/14.
+//
+
+import UIKit
+
+extension UIView {
+    func showLoadingIndicator() {
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        self.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
+    
+    func hideLoadingIndicator() {
+        self.subviews.forEach {
+            if $0 is UIActivityIndicatorView {
+                $0.removeFromSuperview()
+            }
+        }
+    }
+}
