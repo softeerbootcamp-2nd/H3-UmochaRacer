67aa89b8f34d65a0b7ec56da0fca40d26fafcb3e
sangyeon <sangyeun33@gmail.com>
2023-08-14 12:24:14 +0900

    chore: MultiOptionCardButtonViewController - lint 적용 (#92)

--- iOS_H3/iOS_H3_UI/OptionCardButonListView/MultiOptionCardButtonViewController.swift
+++ iOS_H3/iOS_H3_UI/OptionCardButonListView/MultiOptionCardButtonViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class MultiOptionCardButtonViewController: UIViewController {
-    
+
     enum Constants {
         static let inset = 16.0
         static let cardButtonViewHeight = 168.0
@@ -35,7 +35,7 @@ final class MultiOptionCardButtonViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     // MARK: - Properties
     var cardInfos: [OptionCardInfo] = [
         .init(title: "20인치 알로이 휠 & 타이어", subTitle: "구매자의 95%가 선택한", priceString: "+ 0원"),
@@ -45,16 +45,16 @@ final class MultiOptionCardButtonViewController: UIViewController {
     ]
 
     // MARK: - Lifecycles
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupDelegate()
         setupViews()
     }
 
     // MARK: - Helpers
-    
+
     private func setupDelegate() {
         selfModeMultiOptionCardButtonView.delegate = self
     }
@@ -72,16 +72,34 @@ final class MultiOptionCardButtonViewController: UIViewController {
 
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            selfModeMultiOptionCardButtonView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            selfModeMultiOptionCardButtonView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.inset),
-            selfModeMultiOptionCardButtonView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.inset),
+            selfModeMultiOptionCardButtonView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: 10
+            ),
+            selfModeMultiOptionCardButtonView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.inset
+            ),
+            selfModeMultiOptionCardButtonView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -Constants.inset
+            ),
             selfModeMultiOptionCardButtonView.heightAnchor.constraint(equalToConstant: Constants.cardButtonViewHeight)
         ])
         NSLayoutConstraint.activate([
-            guideModeMultiOptionCardButtonView.topAnchor.constraint(equalTo: selfModeMultiOptionCardButtonView.bottomAnchor, constant: 10),
-            guideModeMultiOptionCardButtonView.leadingAnchor.constraint(equalTo: selfModeMultiOptionCardButtonView.leadingAnchor),
-            guideModeMultiOptionCardButtonView.trailingAnchor.constraint(equalTo: selfModeMultiOptionCardButtonView.trailingAnchor),
-            guideModeMultiOptionCardButtonView.heightAnchor.constraint(equalTo: selfModeMultiOptionCardButtonView.heightAnchor)
+            guideModeMultiOptionCardButtonView.topAnchor.constraint(
+                equalTo: selfModeMultiOptionCardButtonView.bottomAnchor,
+                constant: 10
+            ),
+            guideModeMultiOptionCardButtonView.leadingAnchor.constraint(
+                equalTo: selfModeMultiOptionCardButtonView.leadingAnchor
+            ),
+            guideModeMultiOptionCardButtonView.trailingAnchor.constraint(
+                equalTo: selfModeMultiOptionCardButtonView.trailingAnchor
+            ),
+            guideModeMultiOptionCardButtonView.heightAnchor.constraint(
+                equalTo: selfModeMultiOptionCardButtonView.heightAnchor
+            )
         ])
     }
 }
