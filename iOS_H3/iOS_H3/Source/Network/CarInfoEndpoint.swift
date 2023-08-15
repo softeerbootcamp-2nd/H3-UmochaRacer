2d8e40612347dc3a7f0058a6638024691a751a58
eigen98 <terrantrace@gmail.com>
2023-08-15 15:18:11 +0900

    feat : 자동차 정보API 관련 Endpoint 열거형 정의(#127) 1. CarInfoEndpoint 열거형을 사용하여 자동차 정보와 관련된 각 엔드포인트를 정의 2. 각 엔드포인트의 path와 parameters 등을 캡슐화하여 코드의 일관성과 가독성을 높이기 위함.

--- iOS_H3/iOS_H3/Source/Network/CarInfoEndpoint.swift
+++ iOS_H3/iOS_H3/Source/Network/CarInfoEndpoint.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+enum CarInfoEndpoint: Endpoint {
+    case powertrain(model: String, type: String)
+    case drivingSystem
+    case bodyType
+    case exteriorColor
+    case interiorColor
+    case wheel
+    case additionalOption(category: String)
+    case singleExteriorColor(optionId: Int)
+
+    var baseURL: String { ConstantKey.baseURL }
+    var httpMethod: HTTPMethod { .GET }
+    var headers: [String: String] { [:] }
+
+    var path: String {
+        switch self {
+        case .powertrain:
+            return "/powertrain"
+        case .drivingSystem:
+            return "/driving-system"
+        case .bodyType:
+            return "/bodytype"
+        case .exteriorColor:
+            return "/exterior-color"
+        case .interiorColor:
+            return "/interior-color"
+        case .wheel:
+            return "/wheel"
+        case .additionalOption:
+            return "/additional-option"
+        case .singleExteriorColor(let optionId):
+            return "/exterior-color/\(optionId)"
+        }
+    }
+
+    var parameters: HTTPParameter? {
+        switch self {
+        case .powertrain(let model, let type):
+            return .query(["model": model, "type": type])
+        case .additionalOption(let category):
+            return .query(["category": category])
+        default:
+            return nil
+        }
+    }
+}
