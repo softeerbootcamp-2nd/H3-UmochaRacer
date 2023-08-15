ba2f77635359b1692b7d29e980c088d1128c5d74
eigen98 <terrantrace@gmail.com>
2023-08-15 15:34:32 +0900

    feat : BodyTypeResponse 구조체 추가(#127) 1. BodyTypeResponse 구조체 및 내부 BodyType 구조체 정의 2. 필요한 데이터를 담기 위해 id, label, price, iconSrc, imageSrc 속성 추가 3. 차체 유형 정보를 받아오기 위한 Response DTO가 필요하여 해당 구조체를 정의

--- iOS_H3/DTO/CarInfo/BodyTypeResponse.swift
+++ iOS_H3/DTO/CarInfo/BodyTypeResponse.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct BodyTypeResponse: Codable {
+    let data: [BodyType]
+    let message: String?
+    let code: Int?
+
+    struct BodyType: Codable {
+        let id: Int?
+        let label: String?
+        let price: Int?
+        let iconSrc: String?
+        let imageSrc: String?
+    }
+}
