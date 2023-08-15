e5994391581b4531eaa2dbcec90f39b6dd1f4d76
eigen98 <terrantrace@gmail.com>
2023-08-15 15:44:11 +0900

    feat : 자동차 휠 정보 응답을 위한 WheelResponse 구조체 정의 (#127) 1. WheelResponse 구조체 및 내부 Wheel 구조체 추가 2. 응답에 포함될 휠 관련 필드(id, label, price, imageSrc, parts, partsSrc) 정의 3.  자동차 휠 정보를 쉽게 관리하고 사용할 수 있게 함.

--- iOS_H3/DTO/CarInfo/WheelResponse.swift
+++ iOS_H3/DTO/CarInfo/WheelResponse.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct WheelResponse: Codable {
+    let data: [Wheel]
+    let message: String?
+    let code: Int?
+
+    struct Wheel: Codable {
+        let id: Int?
+        let label: String?
+        let price: Int?
+        let imageSrc: String?
+        let parts: String?
+        let partsSrc: String?
+    }
+}
