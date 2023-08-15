185786f65f72add04cbbdf725473f8029c0a5909
eigen98 <terrantrace@gmail.com>
2023-08-15 15:46:42 +0900

    feat : 추가 옵션 응답 모델 정의 (#127) 1. AdditionalOptionResponse 구조체 정의 2. id, name, imageSrc, price, partsSrc 필드를 포함하는 AdditionalOption 내부 구조체 정의 3. fetchAdditionalOption 메서드를 통해 받아온 응답을 파싱하기 위한 모델

--- iOS_H3/DTO/CarInfo/AdditionalOptionResponse.swift
+++ iOS_H3/DTO/CarInfo/AdditionalOptionResponse.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct AdditionalOptionResponse: Codable {
+    let data: [AdditionalOption]
+    let message: String?
+    let code: Int?
+
+    struct AdditionalOption: Codable {
+        let id: Int?
+        let name: String?
+        let imageSrc: String?
+        let price: Int?
+        let partsSrc: String?
+    }
+}
