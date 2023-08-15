eeb65e865eea4b81851515fbf1e14b6381a685cf
Park Seonmi <sm000117@naver.com>
2023-08-15 15:16:29 +0900

    fix: 백카사전 컨트롤러 테스트 수정 (#101)

--- backend/src/test/java/com/example/backend/domain/cardb/controller/CardbControllerTest.java
+++ backend/src/test/java/com/example/backend/domain/cardb/controller/CardbControllerTest.java
@@ -1,35 +1,33 @@
 package com.example.backend.domain.cardb.controller;
 
+import com.example.backend.AbstractRestDocsTest;
 import com.example.backend.domain.cardb.dto.CardbResponseDto;
-import com.example.backend.domain.cardb.repository.CardbRepository;
 import com.example.backend.domain.cardb.service.CardbService;
-import com.example.backend.utils.AbstractRestDocsTest;
-import com.example.backend.utils.RestDocsConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import(RestDocsConfiguration.class)
-@WebMvcTest(CardbController.class)
+
+//@WebMvcTest(CardbController.class)
+@SpringBootTest
 class CardbControllerTest extends AbstractRestDocsTest {
 
     @MockBean
     private CardbService service;
-    @MockBean
-    private CardbRepository repository;
 
     @Test
     @DisplayName("키워드에 해당하는 설명을 반환해야 한다")
@@ -37,24 +35,30 @@ class CardbControllerTest extends AbstractRestDocsTest {
         when(service.getDescriptionByKeyword(any()))
                 .thenReturn(new CardbResponseDto("파워트레인", "토크 어쩌고 저쩌고", "s3 url"));
 
-        mockMvc.perform(get("/api/v1/cardb")
+        ResultActions perform = mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/v1/cardb")
                         .param("keyword", "zzz")
-                )
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andDo(restDocs.document(
-                            requestParameters(
-                                    parameterWithName("keyword").description("백카사전 키워드")
-                            ),
-                                responseFields(
-                                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("상태 코드(Http Status 아님)"),
-                                        fieldWithPath("message").type(JsonFieldType.STRING).description("결과메시지"),
-                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이"),
-                                        fieldWithPath("data.keyword").type(JsonFieldType.STRING).description("백카사전 키워드"),
-                                        fieldWithPath("data.description").type(JsonFieldType.STRING).description("백카사전 설명"),
-                                        fieldWithPath("data.imageSrc").type(JsonFieldType.STRING).description("이미지 url")
-                                )
+        );
+
+        perform.andDo(print())
+                .andExpect(status().isOk());
+
+        perform.andDo(restDocs.document(
+                        "Car DB",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("keyword").description("백카사전 키워드")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("상태 코드(Http Status 아님)"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("결과메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                fieldWithPath("data.keyword").type(JsonFieldType.STRING).description("백카사전 키워드"),
+                                fieldWithPath("data.description").type(JsonFieldType.STRING).description("백카사전 설명"),
+                                fieldWithPath("data.imageSrc").type(JsonFieldType.STRING).description("이미지 url")
                         )
-                );
+                )
+        );
     }
 }
