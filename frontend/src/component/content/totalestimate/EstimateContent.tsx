060ef91c13128eda177632a54962ba69f13cae99
KimJungHun <j7papa@naver.com>
2023-08-15 16:12:04 +0900

    feat: 견적내기 버튼 UI 구현(#112)

--- frontend/src/component/content/totalestimate/EstimateContent.tsx
+++ frontend/src/component/content/totalestimate/EstimateContent.tsx
@@ -2,12 +2,58 @@ import React from 'react';
 import styled from 'styled-components';
 import Top from './estimatecontent/Top';
 import Middle from './estimatecontent/Middle';
+import {colors} from '@/style/theme';
+import {Body2_Medium} from '@/style/fonts';
+
+interface ButtonLayout {
+  text: string;
+  background: string;
+  color: string;
+  border?: string;
+}
+
+const contents: ButtonLayout[] = [
+  {
+    text: '구매 상담 신청',
+    color: colors.Hyundai_White,
+    background: colors.Main_Hyundai_Blue,
+  },
+  {
+    text: '시승 신청하기',
+    color: '#000000',
+    background: colors.Hyundai_White,
+    border: '#000000',
+  },
+  {
+    text: '공유하기',
+    color: colors.Main_Hyundai_Blue,
+    background: colors.Cool_Grey_001,
+  },
+  {
+    text: '저장하기',
+    color: colors.Main_Hyundai_Blue,
+    background: colors.Cool_Grey_001,
+  },
+];
 
 function EstimateContent() {
+  const buttons: React.JSX.Element[] = contents.map((elem, index) => {
+    return (
+      <Button
+        key={index}
+        $background={elem.background}
+        $color={elem.color}
+        $border={elem.border}
+      >
+        {elem.text}
+      </Button>
+    );
+  });
   return (
     <Wrapper>
       <Top></Top>
       <Middle></Middle>
+      <ButtonBox>{buttons}</ButtonBox>
     </Wrapper>
   );
 }
@@ -17,4 +63,27 @@ export default EstimateContent;
 const Wrapper = styled.div`
   width: 100%;
   max-width: 1024px;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 60px;
+  width: 100%;
+  button {
+    ${Body2_Medium}
+  }
+`;
+
+const Button = styled.button<{
+  $background: string;
+  $color: string;
+  $border?: string;
+}>`
+  width: 244px;
+  height: 57px;
+  background: ${({$background}) => $background};
+  color: ${({$color}) => $color};
+  ${({$border}) => $border && `border: 1px solid ${$border}`};
+  border-radius: 6px;
 `;
