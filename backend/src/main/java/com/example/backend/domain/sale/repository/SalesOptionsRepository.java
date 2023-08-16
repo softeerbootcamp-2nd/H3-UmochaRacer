9740b9e73364df9aaf5cf7109e9b32e1766bbcd8
Park Seonmi <sm000117@naver.com>
2023-08-16 13:32:21 +0900

    fix: 테이블 이름 잘못 매칭된 거 수정
    SalesOptionRepository.java -> SalesOptionsRepository

--- backend/src/main/java/com/example/backend/domain/sale/repository/SalesOptionsRepository.java
+++ backend/src/main/java/com/example/backend/domain/sale/repository/SalesOptionsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SalesOptionRepository extends CrudRepository<SalesOptions, Long> {
+public interface SalesOptionsRepository extends CrudRepository<SalesOptions, Long> {
 }
