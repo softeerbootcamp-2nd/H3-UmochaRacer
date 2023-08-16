9c0d50079bf6e4ecb74195c3681ff3fe47774a1c
eigen98 <terrantrace@gmail.com>
2023-08-16 14:21:54 +0900

    feat : 옵션 선택 리스트 뷰 컴포넌트 사용예시 카탈로그 뷰컨트롤러 구현 (#139) 1. `selfModeOptionModeListView`를 생성하여 자동차 제작 모드에 맞는 옵션 뷰를 보여줍니다. 2. 사용자의 옵션 선택과 이미지 모드 버튼 탭에 대한 대응을 구현하였습니다. 3. 뷰 컨트롤러 내에서 뷰 델리게이트를 설정하여 옵션 선택과 이미지 모드 버튼의 액션을 처리하였습니다.

--- iOS_H3/iOS_H3_UI/OptionListView/OptionListModeViewController.swift
+++ iOS_H3/iOS_H3_UI/OptionListView/OptionListModeViewController.swift
@@ -5,4 +5,81 @@
 //  Created by KoJeongMin  on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+final class OptionListModeViewController: UIViewController {
+
+    // MARK: - UI properties
+
+    private lazy var selfModeOptionModeListView: OptionListModeView = {
+        let view = OptionListModeView(carMakingMode: .selfMode)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+
+    // MARK: - Properties
+    var cardInfos: [OptionCardInfo] = [
+        .init(title: "20인치 알로이 휠 & 타이어", subTitle: "구매자의 95%가 선택한", priceString: "+ 0원"),
+            .init(title: "20인치 블랙톤 전면 가공 휠", subTitle: "구매자의 2%가 선택한", priceString: "+ 0원"),
+            .init(title: "가죽 시트", subTitle: "구매자의 60%가 선택한", priceString: "+ 200,000원"),
+            .init(title: "선루프", subTitle: "구매자의 30%가 선택한", priceString: "+ 500,000원"),
+            .init(title: "하이 엔드 오디오 시스템", subTitle: "구매자의 10%가 선택한", priceString: "+ 300,000원"),
+            .init(title: "내비게이션 시스템", subTitle: "구매자의 75%가 선택한", priceString: "+ 100,000원"),
+            .init(title: "후방 카메라", subTitle: "구매자의 50%가 선택한", priceString: "+ 150,000원"),
+            .init(title: "히터 내장 사이드 미러", subTitle: "구매자의 20%가 선택한", priceString: "+ 50,000원"),
+            .init(title: "자동 에어컨", subTitle: "구매자의 40%가 선택한", priceString: "+ 250,000원"),
+            .init(title: "헤드 업 디스플레이", subTitle: "구매자의 15%가 선택한", priceString: "+ 400,000원"),
+            .init(title: "무선 충전 패드", subTitle: "구매자의 25%가 선택한", priceString: "+ 80,000원")
+    ]
+
+    // MARK: - Lifecycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupDelegate()
+        setupViews()
+        self.selfModeOptionModeListView.updateAllViews(with: cardInfos)
+    }
+
+    // MARK: - Helpers
+
+    private func setupDelegate() {
+        selfModeOptionModeListView.delegate = self
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .white
+        addSubviews()
+        setupConstraints()
+    }
+
+    private func addSubviews() {
+        view.addSubview(selfModeOptionModeListView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            selfModeOptionModeListView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor),
+            selfModeOptionModeListView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor),
+            selfModeOptionModeListView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor),
+            selfModeOptionModeListView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+         
+    }
+}
+
+extension OptionListModeViewController: OptionListModeViewDelegate {
+    func optionListModeView(didSelectedIndex: Int) {
+        cardInfos[didSelectedIndex].isSelected.toggle()
+        selfModeOptionModeListView.updateAllViews(with: cardInfos)
+    }
+
+    func optionListModeViewDidTapImageModeButton() {
+        self.dismiss(animated: true)
+    }
+}
