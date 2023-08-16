8029bc1936567819eb1eda1a58368928dc457902
sangyeon <sangyeun33@gmail.com>
2023-08-16 12:10:41 +0900

    chore: SceneDelegate - CarMakingViewController init 시 viewModel 전달 (#132)

--- iOS_H3/iOS_H3/Source/App/SceneDelegate.swift
+++ iOS_H3/iOS_H3/Source/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = CarMakingViewController(mode: .selfMode)
+        window?.rootViewController = CarMakingViewController(mode: .selfMode, viewModel: CarMakingViewModel())
         window?.makeKeyAndVisible()
     }
 
