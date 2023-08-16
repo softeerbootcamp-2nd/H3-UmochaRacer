f11ee7694715d60c57c7b004c4c948f0d53efdf5
sangyeon <sangyeun33@gmail.com>
2023-08-16 12:06:16 +0900

    feat: CarMakingViewModel 구현 & 내차만들기 단계 변화 시 해당 단계에 대한 정보를 ViewController에 전달 (#132)

---
+++ iOS_H3/iOS_H3/Source/Presentation/CarMaking/ViewModel/CarMakingViewModel.swift
@@ -0,0 +1,130 @@
+//
+//  CarMakingViewModel.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/15.
+//
+
+import Foundation
+import Combine
+
+final class CarMakingViewModel {
+
+    // MARK: - Input
+
+    struct Input {
+        var carMakingStepDidChanged: CurrentValueSubject<CarMakingStep, Never>
+    }
+
+    // MARK: - Output
+
+    struct Output {
+        var currentStepInfo = CurrentValueSubject<CarMakingStepInfo, Never>(CarMakingStepInfo(step: .powertrain))
+    }
+
+    // MARK: - Properties
+
+    private var cancellables = Set<AnyCancellable>()
+
+    // MARK: - Dependency
+
+    // MARK: - Lifecycles
+
+    // MARK: - Helpers
+
+    func transform(_ input: Input) -> Output {
+        let output = Output()
+
+        input.carMakingStepDidChanged
+            .sink(receiveValue: { step in
+                // usecase에 데이터 요청. ex) usecase.getStepInfo(of: stepIndex)
+                // 전달된 옵션들 중 디폴트로 선택된 옵션의 isSelected 값 수정해서 전달
+                // 그러기 위해선 viewDidLoad 시점에 받은 견적요약 데이터 저장해놓고 수정하기? 근데 이건 usecase에서 해야할 거 같기도
+
+                let stepIndex = step.rawValue
+                let carMakingStepInfo = CarMakingStepInfo(
+                    step: step,
+                    bannerImageURL: CarMakingMockData.mockURL[stepIndex],
+                    optionCardInfoArray: CarMakingMockData.mockOption[stepIndex]
+                )
+                output.currentStepInfo.send(carMakingStepInfo)
+            })
+            .store(in: &cancellables)
+
+        return output
+    }
+}
+
+struct CarMakingMockData {
+    static let mockURL = [
+        "https://cdn.autotribune.co.kr/news/photo/202101/4849_30727_3533.jpg",
+         // swiftlint:disable:next line_length
+         "https://i.namu.wiki/i/cBMX6XiTLltPPIawbb2zfP5Oy5RW9JybY0E5ZQ62oUYdppA2t54xDjiST7xfLe_2dL4pGN9VsBVknq4H-SYA2A.webp",
+         "https://cdn.epnc.co.kr/news/photo/201804/79474_70575_4841.jpg",
+         "https://pimg.daara.co.kr/kidd/photo/2021/01/08/thumbs/thumb_520390_1610089982_79.jpg",
+        "https://pimg.daara.co.kr/kidd/photo/2021/01/08/thumbs/thumb_520390_1610089971_11.jpg",
+         "https://itimg.chosun.com/sitedata/image/202112/03/2021120301496_0.jpg",
+         "https://www.hyundai-wia.com/img/business/auto_engine_detail_img_5.jpg",
+         "https://www.hyundai-wia.com/img/business/auto_engine_detail_img_5.jpg",
+         "https://www.hyundai-wia.com/img/business/auto_engine_detail_img_5.jpg"
+    ].map { URL(string: $0) }
+
+    static let mockOption = [
+            [OptionCardInfo.init(title: "디젤 2.2",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 37%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "2WD",
+                                 subTitle: "구매자의 72%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "4WD",
+                                 subTitle: "구매자의 82%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "7인승",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "8인승",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "LK-99 3.8",
+                                 subTitle: "구매자의 88%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "LK-99 3.8",
+                                 subTitle: "구매자의 12%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 3,456,789원")],
+            [OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 0 원",
+                                 isSelected: true),
+             OptionCardInfo.init(title: "가솔린 3.8",
+                                 subTitle: "구매자의 63%가 선택한",
+                                 priceString: "+ 0 원")]
+    ]
+}
