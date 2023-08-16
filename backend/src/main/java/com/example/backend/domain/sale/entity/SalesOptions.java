41e750afd92a77610954d5e7a348b5c1e438b096
Park Seonmi <sm000117@naver.com>
2023-08-16 23:56:20 +0900

    fix: 테이블 참조 필드 수정 (#121)

--- backend/src/main/java/com/example/backend/domain/sale/entity/SalesOptions.java
+++ backend/src/main/java/com/example/backend/domain/sale/entity/SalesOptions.java
@@ -2,17 +2,13 @@ package com.example.backend.domain.sale.entity;
 
 import com.example.backend.domain.information.model.option.entity.AdditionalOption;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.relational.core.mapping.Table;
-
-import java.util.List;
 
 @Table("SALES_OPTIONS")
 public class SalesOptions {
     @Id
     private Long id;
-    @MappedCollection(keyColumn = "id")
-    private List<Sales> salesId;
-    @MappedCollection(keyColumn = "id")
-    private List<AdditionalOption> optionId;
+    private AggregateReference<Sales, Long> salesId;
+    private AggregateReference<AdditionalOption, Long> optionId;
 }
