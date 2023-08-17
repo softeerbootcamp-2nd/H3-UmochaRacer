21475a27b704848f2b46036fa108824946222c5a
eigen98 <terrantrace@gmail.com>
2023-08-18 08:41:03 +0900

    feat : URCOlor hex를 매개변수로 받는 생성자 추가(#154) 1. 자동차 옵션의 색상을 16진수 문자열로 받아 처리해야 하는 경우가 있습니다. 이를 위해 URColor 구조체에 16진수 색상 코드를 파라미터로 받는 초기화 메서드를 추가

--- iOS_H3/iOS_H3/Source/Domain/Model/URColor.swift
+++ iOS_H3/iOS_H3/Source/Domain/Model/URColor.swift
@@ -11,4 +11,26 @@ struct URColor: Hashable {
     let red: Int8
     let green: Int8
     let blue: Int8
+
+    init(red: Int8, green: Int8, blue: Int8) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+
+    init(hex: String) {
+        let hex = hex.hasPrefix("#") ? String(hex.dropFirst()) : hex
+        let scanner = Scanner(string: hex)
+        var rgb: UInt64 = 0
+
+        if scanner.scanHexInt64(&rgb) {
+            let red = Int8((rgb & 0xFF0000) >> 16)
+            let green = Int8((rgb & 0x00FF00) >> 8)
+            let blue = Int8(rgb & 0x0000FF)
+
+            self.init(red: red, green: green, blue: blue)
+        } else {
+            self.init(red: 0, green: 0, blue: 0)
+        }
+    }
 }
