4e535431a4f61386763def92c7ad4d95f28510e5
eigen98 <terrantrace@gmail.com>
2023-08-18 08:37:02 +0900

    feat : 메서드 반환 타입 수정 및 견적 조회 요청 메서드 추가 (#154) 1. 반환 타입을 차량 옵션 정보의 DTO 타입에서 Entity타입으로 수정하였습니다. 2. 견적 조회 메서드를 추가하였습니다.₩

--- iOS_H3/iOS_H3/Source/Extensions/CarInfoRepositoryProtocol.swift
+++ iOS_H3/iOS_H3/Source/Extensions/CarInfoRepositoryProtocol.swift
@@ -9,19 +9,22 @@ import Foundation
 import Combine
 
 protocol CarInfoRepositoryProtocol {
-    func fetchPowertrain(model: String, type: String) -> AnyPublisher<Result<PowertrainResponse, Error>, Never>
+    typealias APIResult<T> = AnyPublisher<Result<T, Error>, Never>
+    func fetchPowertrain(model: String, type: String) -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchDrivingSystem() -> AnyPublisher<Result<DrivingSystemResponse, Error>, Never>
+    func fetchDrivingSystem() -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchBodyType() -> AnyPublisher<Result<BodyTypeResponse, Error>, Never>
+    func fetchBodyType() -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchExteriorColor() -> AnyPublisher<Result<ExteriorColorResponse, Error>, Never>
+    func fetchExteriorColor() -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchInteriorColor() -> AnyPublisher<Result<InteriorColorResponse, Error>, Never>
+    func fetchInteriorColor() -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchWheel() -> AnyPublisher<Result<WheelResponse, Error>, Never>
+    func fetchWheel() -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchAdditionalOption(category: String) -> AnyPublisher<Result<AdditionalOptionResponse, Error>, Never>
+    func fetchAdditionalOption(category: String) -> AnyPublisher<CarMakingStepInfo, Never>
 
-    func fetchSingleExteriorColor(optionId: Int) -> AnyPublisher<Result<SingleExteriorColorResponse, Error>, Never>
+    func fetchSingleExteriorColor(optionId: Int) -> AnyPublisher<CarMakingStepInfo, Never>
+    // TODO: Intro Repository 분리
+    func fetchEstimate() -> AnyPublisher<EstimateSummary, Never>
 }
