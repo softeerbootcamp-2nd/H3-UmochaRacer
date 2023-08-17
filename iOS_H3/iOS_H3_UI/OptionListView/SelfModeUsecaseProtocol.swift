46c43d7c891dba5265422b592a1aa24a3176f68d
eigen98 <terrantrace@gmail.com>
2023-08-18 08:42:53 +0900

    feat : 자동차 옵션 관리를 위한 SelfModeUsecase 및 SelfModeUsecaseProtocol 구현(#154) 1. 자동차 제작 과정에서 다양한 옵션을 관리하고 선택할 수 있어야 하므로, 이를 위한 Usecase 클래스와 프로토콜을 추가하였습니다. 2. `SelfModeUsecaseProtocol`: 자동차 제작에 필요한 옵션을 가져오는 메서드들을 정의합니다. - `SelfModeUsecase`: 프로토콜을 구현하며, 각 단계별로 해당하는 옵션을 가져오는 로직을 포함하고 있습니다. - `fetchInitialEstimate`: 초기 견적을 가져오는 메서드입니다. - `fetchOptionInfo(step: CarMakingStep)`: 주어진 단계에 해당하는 옵션 정보를 가져옵니다.

--- iOS_H3/iOS_H3_UI/OptionListView/SelfModeUsecaseProtocol.swift
+++ iOS_H3/iOS_H3_UI/OptionListView/SelfModeUsecaseProtocol.swift
@@ -5,4 +5,70 @@
 //  Created by KoJeongMin  on 2023/08/16.
 //
 
+import Combine
 import Foundation
+
+protocol SelfModeUsecaseProtocol {
+     func fetchInitialEstimate() -> AnyPublisher<EstimateSummary, Never>
+
+    func fetchOptionInfo(step: CarMakingStep) -> AnyPublisher<CarMakingStepInfo, Never>
+
+    // func applyOptionToEstimate(option: OptionCardInfo) -> AnyPublisher<Result<[EstimateSummary], Error>, Never>
+}
+
+class SelfModeUsecase: SelfModeUsecaseProtocol {
+
+    let carInfoRepository: CarInfoRepositoryProtocol
+
+    init(carInfoRepository: CarInfoRepositoryProtocol) {
+        self.carInfoRepository = carInfoRepository
+    }
+
+    func fetchInitialEstimate() -> AnyPublisher<EstimateSummary, Never> {
+        let publisher = carInfoRepository.fetchEstimate()
+        return publisher
+            .map { summary in
+                var mutableSummary = summary
+                return mutableSummary
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func fetchOptionInfo(step: CarMakingStep) -> AnyPublisher<CarMakingStepInfo, Never> {
+        let publisher: AnyPublisher<CarMakingStepInfo, Never>
+
+        switch step {
+        case .powertrain:
+            publisher = carInfoRepository.fetchPowertrain(model: "asdf", type: "타입명")
+        case .driveMethod:
+            publisher = carInfoRepository.fetchDrivingSystem()
+        case .bodyType:
+            publisher = carInfoRepository.fetchBodyType()
+        case .externalColor:
+            publisher = carInfoRepository.fetchExteriorColor()
+        case .internalColor:
+            publisher = carInfoRepository.fetchInteriorColor()
+        case .wheelSelection:
+            publisher = carInfoRepository.fetchWheel()
+        case .optionSelection:
+            publisher = carInfoRepository.fetchAdditionalOption(category: "")
+        default:
+            publisher = Just(CarMakingStepInfo(step: step)).eraseToAnyPublisher()
+        }
+
+        return publisher
+            .map { stepInfo in
+                var mutableStepInfo = stepInfo
+                if !mutableStepInfo.optionCardInfoArray.isEmpty {
+                    mutableStepInfo.optionCardInfoArray[0].isSelected = true
+                }
+                return mutableStepInfo
+            }
+            .eraseToAnyPublisher()
+    }
+
+}
+
+//    func applyOptionToEstimate(option: OptionCardInfo) -> AnyPublisher<Result<[EstimateSummary], Error>, Never> {
+//
+//    }
