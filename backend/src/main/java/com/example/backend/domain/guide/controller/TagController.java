493233a6edf961fd123def6658d4321e4d3fb3f2
jjy0709 <jjy0709@kaist.ac.kr>
2023-08-18 23:33:25 +0900

    feature: 사용자 정보(나이, 연령, 태그 3개)에 따라 옵션을 추천해주는 API 구현(#123)

--- backend/src/main/java/com/example/backend/domain/guide/controller/TagController.java
+++ backend/src/main/java/com/example/backend/domain/guide/controller/TagController.java
@@ -2,14 +2,15 @@ package com.example.backend.domain.guide.controller;
 
 import com.example.backend.domain.global.dto.ResponseDto;
 import com.example.backend.domain.global.model.enums.ResultCode;
+import com.example.backend.domain.guide.dto.EstimateRequest;
+import com.example.backend.domain.guide.dto.FinalEstimateResponse;
 import com.example.backend.domain.guide.dto.TagListResponse;
+import com.example.backend.domain.guide.service.JYCarRecommendationService;
 import com.example.backend.domain.guide.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +19,21 @@ import java.util.List;
 @RequestMapping("/api/v1/guide")
 public class TagController {
     private final TagService tagService;
+    private final JYCarRecommendationService jyCarRecommendationService;
 
     @GetMapping("/tag")
     public ResponseEntity<ResponseDto<List<TagListResponse>>> returnAllTagWithCategory() {
         List<TagListResponse> results = tagService.returnAllTag();
         ResponseDto<List<TagListResponse>> body = ResponseDto.of(results, ResultCode.SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDto<FinalEstimateResponse>> returnGuideOption(
+            @RequestBody EstimateRequest request
+    ) {
+        FinalEstimateResponse result = jyCarRecommendationService.estimate(request);
+        ResponseDto<FinalEstimateResponse> body = ResponseDto.of(result, ResultCode.SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 }
---
+++ backend/src/main/java/com/example/backend/domain/guide/repository/TagOptionRepository.java
@@ -0,0 +1,41 @@
+package com.example.backend.domain.guide.repository;
+
+import com.example.backend.domain.sale.entity.enums.Gender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class TagOptionRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Long> findOptionIdByTagId(String category, Long tagId) {
+        String query = String.format("SELECT %s_id FROM TAG_%s WHERE tag_id = %d", category, category.toUpperCase(), tagId);
+        return jdbcTemplate.query(query, getRowMapperOfField(category));
+    }
+
+    private RowMapper<Long> getRowMapperOfField(String field) {
+        return new RowMapper<Long>() {
+            @Override
+            public Long mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return rs.getLong(field + "_id");
+            }
+        };
+    }
+
+    public List<Long> findOptionIdByGenderAge(String category, Gender gender, int age) {
+        String category_id = category + "_id";
+        String query = "SELECT m." + category_id + ", COUNT(*) as select_count FROM SALES s\n" +
+                "JOIN MODEL m on s.model_id = m.id WHERE m.trim_id = 1 AND\n" +
+                "s.gender = \'" + gender.name() + "\' AND "+ age + "<= s.age <" + (age+10) +
+                " GROUP BY m." + category_id + " ORDER BY select_count DESC LIMIT 1";
+
+        return jdbcTemplate.query(query, getRowMapperOfField(category));
+    }
+}
