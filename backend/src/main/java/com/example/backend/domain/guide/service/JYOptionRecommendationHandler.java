08e9783f08fcff1cf43f5a4822f5d57a42a18291
jjy0709 <jjy0709@kaist.ac.kr>
2023-08-18 23:33:25 +0900

    feature: 사용자 정보(나이, 연령, 태그 3개)에 따라 옵션을 추천해주는 API 구현(#123)

---
+++ backend/src/main/java/com/example/backend/domain/guide/service/JYOptionRecommendationHandler.java
@@ -0,0 +1,31 @@
+package com.example.backend.domain.guide.service;
+
+import com.example.backend.domain.guide.dto.EstimateRequest;
+import com.example.backend.domain.guide.repository.TagOptionRepository;
+import com.example.backend.domain.sale.entity.enums.Gender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JYOptionRecommendationHandler implements OptionRecommendationHandler {
+    private final TagOptionRepository tagOptionRepository;
+
+    @Override
+    public List<Long> findByTag(String category, List<Long> tagIds) {
+        for(Long tagId: tagIds) {
+            List<Long> optionList = tagOptionRepository.findOptionIdByTagId(category, tagId);
+            if(optionList.size() != 0)
+                return optionList;
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Long> findTopSalesCount(String category, Gender gender, int age) {
+        return tagOptionRepository.findOptionIdByGenderAge(category, gender, age);
+    }
+}
