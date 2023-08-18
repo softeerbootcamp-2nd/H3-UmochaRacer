9f66000836daada544e3431f2b79868d7aa60731
sangyeon <sangyeun33@gmail.com>
2023-08-18 10:41:57 +0900

    feat: OptionCardButtonListViewDelegate 구현 (#156)
    - 옵션 카드가 선택되었을 때 알려주는 didSelectOptionAt
    - 옵션 카드가 보여졌을 때 알려주는 didDisplayOptionAt

--- iOS_H3/iOS_H3_UI/OptionCardButonListView/OptionCardButtonListViewable.swift
+++ iOS_H3/iOS_H3_UI/OptionCardButonListView/OptionCardButtonListViewable.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+protocol OptionCardButtonListViewDelegate: AnyObject {
+    func optionCardButtonListView(
+        _ optionCardButtonListView: OptionCardButtonListViewable,
+        didSelectOptionAt index: Int
+    )
+    func optionCardButtonListView(
+        _ optionCardButtonListView: OptionCardButtonListViewable,
+        didDisplayOptionAt index: Int
+    )
+}
+
 protocol OptionCardButtonListViewable: UIView {
     func configure(with cardInfos: [OptionCardInfo])
 }
