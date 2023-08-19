085ecbd96a27a7658b6869895a432aafa47cb3ea
eigen98 <terrantrace@gmail.com>
2023-08-19 14:01:28 +0900

    feat : CarInfoRepository 에러 유형 정의 추가 - Car 정보 저장소에서 발생할 수 있는 에러 유형을 enum으로 정의함. - 네트워크 오류와 변환 오류 두 가지 주요 유형을 포함함. - 네트워크 오류는 내부 에러를 포함하며, 변환 오류는 CarOptionToEntityError를 사용하여 구체적인 변환 문제를 설명함. - 각 에러 유형에 대한 설명을 LocalizedError 프로토콜을 통해 제공함.

--- iOS_H3/iOS_H3/Source/Extensions/CarInfoRepositoryProtocol.swift
+++ iOS_H3/iOS_H3/Source/Extensions/CarInfoRepositoryProtocol.swift
@@ -8,23 +8,38 @@
 import Foundation
 import Combine
 
+enum CarInfoRepositoryError: LocalizedError {
+    case networkError(Error)
+    case conversionError(CarOptionToEntityError)
+
+    var errorDescription: String? {
+        switch self {
+        case .networkError(let error):
+            return "네트워크 오류: \(error.localizedDescription)"
+        case .conversionError(let error):
+            return "변환 오류: \(error.localizedDescription)"
+        }
+    }
+}
+
+
 protocol CarInfoRepositoryProtocol {
     typealias APIResult<T> = AnyPublisher<Result<T, Error>, Never>
-    func fetchPowertrain(model: String, type: String) -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchPowertrain(model: String, type: String) -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchDrivingSystem() -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchDrivingSystem() -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchBodyType() -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchBodyType() -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchExteriorColor() -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchExteriorColor() -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchInteriorColor() -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchInteriorColor() -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchWheel() -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchWheel() -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchAdditionalOption(category: String) -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchAdditionalOption(category: String) -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
 
-    func fetchSingleExteriorColor(optionId: Int) -> AnyPublisher<CarMakingStepInfo, Never>
+    func fetchSingleExteriorColor(optionId: Int) -> AnyPublisher<CarMakingStepInfo, CarInfoRepositoryError>
     // TODO: Intro Repository 분리
     func fetchEstimate() -> AnyPublisher<EstimateSummary, Never>
 }
