124f014756ee6e3dd573d88563864138a28a4030
sangyeon <sangyeun33@gmail.com>
2023-08-19 22:19:04 +0900

    design: CarMakingContentViewController - 옵션 선택 시 옵션 isSelected 수정 후 화면에 반영

--- iOS_H3/iOS_H3_UI/CarMakingContentView/CarMakingContentViewController.swift
+++ iOS_H3/iOS_H3_UI/CarMakingContentView/CarMakingContentViewController.swift
@@ -102,6 +102,7 @@ extension CarMakingContentViewController: CarMakingContentViewDelegate {
 
     func carMakingContentView(optionDidSelectedAt optionIndex: Int, in stepIndex: Int) {
         CarMakingContentMockData.mockOption[stepIndex][optionIndex].isSelected.toggle()
+        carMakingContentView.updateOptionCard(with: CarMakingContentMockData.mockOption[stepIndex])
     }
 
     func carMakingContentView(categoryDidSelected category: OptionCategoryType) {
