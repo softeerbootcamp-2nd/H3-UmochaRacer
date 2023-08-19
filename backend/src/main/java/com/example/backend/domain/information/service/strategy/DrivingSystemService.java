299d52250abc89a41d92f7d1623d50406ecda56d
Park Seonmi <sm000117@naver.com>
2023-08-19 13:54:25 +0900

    rename: DrivingSystemService의 변수명 변경
    repository -> drivingSystemRepository

--- backend/src/main/java/com/example/backend/domain/information/service/strategy/DrivingSystemService.java
+++ backend/src/main/java/com/example/backend/domain/information/service/strategy/DrivingSystemService.java
@@ -5,7 +5,6 @@ import com.example.backend.domain.global.model.enums.ResultCode;
 import com.example.backend.domain.information.dto.CommentResponse;
 import com.example.backend.domain.information.dto.CommonResponse;
 import com.example.backend.domain.information.mapper.InformationMapper;
-import com.example.backend.domain.information.model.option.entity.Bodytype;
 import com.example.backend.domain.information.model.option.entity.DrivingSystem;
 import com.example.backend.domain.information.model.option.repository.DrivingSystemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,18 +17,18 @@ import java.util.stream.Collectors;
 @Component
 @RequiredArgsConstructor
 public class DrivingSystemService implements InformationStrategy {
-    private final DrivingSystemRepository repository;
+    private final DrivingSystemRepository drivingSystemRepository;
     private final InformationMapper informationMapper;
 
     @Override
     public List<CommonResponse> findAll() {
-        List<DrivingSystem> all = (List<DrivingSystem>) repository.findAll();
+        List<DrivingSystem> all = (List<DrivingSystem>) drivingSystemRepository.findAll();
         return all.stream().map(informationMapper::map).collect(Collectors.toList());
     }
 
     @Override
     public CommonResponse findInformationById(long id) {
-        Optional<DrivingSystem> target = repository.findById(id);
+        Optional<DrivingSystem> target = drivingSystemRepository.findById(id);
         if(target.isEmpty()) throw new RestApiException(ResultCode.NO_CAR_INFORMATION_WITH_ID);
         return informationMapper.map(target.get());
     }
@@ -41,7 +40,7 @@ public class DrivingSystemService implements InformationStrategy {
 
     @Override
     public CommentResponse findCommentById(long id) {
-        String comment = repository.findDrivingSystemCommentById(id);
+        String comment = drivingSystemRepository.findDrivingSystemCommentById(id);
         if (comment == null) throw new RestApiException(ResultCode.NO_COMMENT_EXIST_FOR_ID);
         return CommentResponse.builder()
                 .comment(comment)
@@ -50,8 +49,6 @@ public class DrivingSystemService implements InformationStrategy {
 
     @Override
     public Long findDetailId(long id) {
-        return repository.findDetailIdById(id);
+        return drivingSystemRepository.findDetailIdById(id);
     }
-
-
 }
