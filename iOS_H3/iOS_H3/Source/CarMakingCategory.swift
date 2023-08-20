b050e6e192256725d2ede0856183dfba0c7ae0c5
sangyeon <sangyeun33@gmail.com>
2023-08-20 23:42:32 +0900

    feat: CarMakingCategory - title 프로퍼티 추가 (#183)
    - 견적요약 collectionview의 섹션 헤더에서 카테고리 이름을 나타내기 위해 구현

--- iOS_H3/iOS_H3/Source/CarMakingCategory.swift
+++ iOS_H3/iOS_H3/Source/CarMakingCategory.swift
@@ -11,4 +11,15 @@ enum CarMakingCategory: String, CaseIterable {
     case defaultInfo = "car"
     case colorInfo = "color"
     case optionInfo = "additional-option"
+
+    var title: String {
+        switch self {
+        case .defaultInfo:
+            return "팰리세이드 Le Blanc (르블랑)"
+        case .colorInfo:
+            return "색상"
+        case .optionInfo:
+            return "옵션"
+        }
+    }
 }
