38d0613b3a88be37ad6c9c894d456d78b7b55a13
sangyeon <sangyeun33@gmail.com>
2023-08-20 20:17:33 +0900

    feat: EstimateSummaryView - 견적 정보 나타내기 위한 collectionView 구현 (#183)
    - diffableDatasoure, flowLayout 이용
    - diffableDatasoure의 섹션 타입은 CarMakingCategory, 아이템 타입은 EstimateSummaryElement

--- iOS_H3/iOS_H3_UI/EstimateSummaryView/EstimateSummaryView.swift
+++ iOS_H3/iOS_H3_UI/EstimateSummaryView/EstimateSummaryView.swift
@@ -13,13 +13,28 @@ final class EstimateSummaryView: UIScrollView {
         static let titleLabelTopOffset = 17.5
         static let labelSideOffset = 20.0
         static let labelHeight = 26.0
+
+        static let collectionViewCellHeight = 18.0
+        static let collectionViewHeaderHeight = 38.0
+        static let collectionViewSectionInset = UIEdgeInsets(top: 27, left: 0, bottom: 27, right: 0)
+        static let collectionViewTopOffset = 36.0
     }
+
+    typealias DiffableDataSource = UICollectionViewDiffableDataSource<CarMakingCategory, EstimateSummaryElement>
 
     // MARK: - UI properties
 
     private let titleLabel = UILabel()
 
     private let priceLabel = UILabel()
+
+    private var collectionView: UICollectionView!
+
+    // MARK: - Properties
+
+    private lazy var screenSize = window?.windowScene?.screen.bounds.size ?? UIWindow().screen.bounds.size
+
+    private var dataSource: DiffableDataSource!
 
     // MARK: - Lifecycles
 
@@ -42,7 +57,23 @@ final class EstimateSummaryView: UIScrollView {
     }
 }
 
-// MARK: - Setup
+extension EstimateSummaryView {
+
+    private func updateSnapshot(item: [EstimateSummaryElement]) {
+        var snapshot = dataSource.snapshot()
+
+        let previousItem = snapshot.itemIdentifiers
+        snapshot.deleteItems(previousItem)
+
+        item.forEach { element in
+            snapshot.appendItems([element], toSection: element.category)
+        }
+
+        dataSource.apply(snapshot)
+    }
+}
+
+// MARK: - Setup Views
 
 extension EstimateSummaryView {
 
@@ -52,22 +83,12 @@ extension EstimateSummaryView {
         setupConstraints()
     }
 
+    // MARK: - Setup UIProperties
+
     private func setupUIProperties() {
         setupTitleLabel()
         setupPriceLabel()
-    }
-
-    private func addSubviews() {
-        [titleLabel, priceLabel].forEach { addSubview($0) }
-    }
-
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            contentLayoutGuide.leadingAnchor.constraint(equalTo: leadingAnchor),
-            contentLayoutGuide.trailingAnchor.constraint(equalTo: trailingAnchor)
-        ])
-        setupTitleLabelConstraints()
-        setupPriceLabelConstraints()
+        setupCollectionView()
     }
 
     private func setupTitleLabel() {
@@ -84,6 +105,65 @@ extension EstimateSummaryView {
         priceLabel.translatesAutoresizingMaskIntoConstraints = false
     }
 
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createFlowLayout())
+        collectionView.isScrollEnabled = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        collectionView.register(EstimateSummaryCell.self, forCellWithReuseIdentifier: EstimateSummaryCell.identifier)
+        setupDataSourceOfCollectionView()
+        setupSnapshot()
+    }
+
+    private func createFlowLayout() -> UICollectionViewFlowLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: screenSize.width, height: Constants.collectionViewCellHeight)
+        flowLayout.sectionInset = Constants.collectionViewSectionInset
+        return flowLayout
+    }
+
+    private func setupDataSourceOfCollectionView() {
+        dataSource = DiffableDataSource(
+            collectionView: collectionView
+        ) { (collectionView, indexPath, item) in
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: EstimateSummaryCell.identifier,
+                for: indexPath
+            ) as? EstimateSummaryCell else {
+                return EstimateSummaryCell()
+            }
+
+            cell.configure(item)
+
+            return cell
+        }
+    }
+
+    private func setupSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<CarMakingCategory, EstimateSummaryElement>()
+        snapshot.appendSections(CarMakingCategory.allCases)
+        dataSource.apply(snapshot)
+    }
+
+    // MARK: - AddSubviews
+
+    private func addSubviews() {
+        [titleLabel, priceLabel, collectionView].forEach { addSubview($0) }
+    }
+
+    // MARK: - Setup Constraints
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            contentLayoutGuide.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentLayoutGuide.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentLayoutGuide.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        setupTitleLabelConstraints()
+        setupPriceLabelConstraints()
+        setupCollectionViewConstraints()
+    }
+
     private func setupTitleLabelConstraints() {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Constants.titleLabelTopOffset),
@@ -97,6 +177,18 @@ extension EstimateSummaryView {
             priceLabel.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
             priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.labelSideOffset),
             priceLabel.heightAnchor.constraint(equalTo: titleLabel.heightAnchor)
+        ])
+    }
+
+    private func setupCollectionViewConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(
+                equalTo: titleLabel.bottomAnchor,
+                constant: Constants.collectionViewTopOffset
+            ),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 }
