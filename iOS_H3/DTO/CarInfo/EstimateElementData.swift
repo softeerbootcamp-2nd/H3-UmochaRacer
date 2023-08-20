92bac6068c92ce2e3f28d15e1dab0972e0d7277c
sangyeon <sangyeun33@gmail.com>
2023-08-20 20:07:59 +0900

    feat: EstimateElementData의 toDomain() 수정 (#183)
    - CarMakingCategory 구현에 따라 category string으로부터 CarMakingCategory 생성하여 EstimateSummaryElement로 전환
    - 서버 응답에 문제가 있는 관계로 임시코드로 작성하고, 실제 사용하게 될 코드는 주석 처리함

--- iOS_H3/DTO/CarInfo/EstimateElementData.swift
+++ iOS_H3/DTO/CarInfo/EstimateElementData.swift
@@ -12,6 +12,7 @@ enum EstimateElementToEntityError: LocalizedError {
     case missingKey
     case missingValue
     case negativePrice
+    case invalidCategory
 
     var errorDescription: String? {
         switch self {
@@ -23,6 +24,8 @@ enum EstimateElementToEntityError: LocalizedError {
             return "타입 값이 없습니다."
         case .negativePrice:
             return "가격 값이 음수일 수 없습니다."
+        case .invalidCategory:
+            return "유효하지 않은 카테고리입니다."
         }
     }
 }
@@ -49,9 +52,20 @@ extension EstimateElementData {
             throw EstimateElementToEntityError.negativePrice
         }
 
-        return EstimateSummaryElement(stepName: key,
-                                      selectedOption: value,
-                                      category: category,
+        #warning("서버 응답에 따른 임시 코드입니다. 서버 수정 되면 주석 코드로 수정 필요")
+        guard let carMakingCategory = CarMakingCategory(rawValue: value) else {
+            throw EstimateElementToEntityError.invalidCategory
+        }
+        return EstimateSummaryElement(stepName: category,
+                                      selectedOption: key,
+                                      category: carMakingCategory,
                                       price: price)
+//        guard let carMakingCategory = CarMakingCategory(rawValue: category) else {
+//            throw EstimateElementToEntityError.invalidCategory
+//        }
+//        return EstimateSummaryElement(stepName: key,
+//                                      selectedOption: value,
+//                                      category: carMakingCategory,
+//                                      price: price)
     }
 }
