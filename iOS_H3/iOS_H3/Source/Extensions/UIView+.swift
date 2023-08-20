29330d2a4eef58c1b859be9812994421d7281afa
eigen98 <terrantrace@gmail.com>
2023-08-20 18:56:03 +0900

    feat : 인터페이스 빌더에서 사용할 컬러 인스펙터 속성 추가 (#184) 1. UIView에 대한 확장을 통해 인터페이스 빌더의 인스펙터 창에서 직접 컬러를 선택할 수 있게 하였습니다. 2. 이를 통해 더 편리하게 디자인을 적용할 수 있게 되었습니다.

--- iOS_H3/iOS_H3/Source/Extensions/UIView+.swift
+++ iOS_H3/iOS_H3/Source/Extensions/UIView+.swift
@@ -27,3 +27,60 @@ extension UIView {
         }
     }
 }
+
+extension UIView {
+    @IBInspectable var mainHyundaiBlue: Bool {
+           get { return backgroundColor == Colors.mainHyundaiBlue }
+           set { backgroundColor = newValue ? Colors.mainHyundaiBlue : backgroundColor }
+       }
+
+       @IBInspectable var subActiveBlue: Bool {
+           get { return backgroundColor == Colors.subActiveBlue }
+           set { backgroundColor = newValue ? Colors.subActiveBlue : backgroundColor }
+       }
+
+       @IBInspectable var iconYellow: Bool {
+           get { return backgroundColor == Colors.iconYellow }
+           set { backgroundColor = newValue ? Colors.iconYellow : backgroundColor }
+       }
+
+       @IBInspectable var tagSkyBlue: Bool {
+           get { return backgroundColor == Colors.tagSkyBlue }
+           set { backgroundColor = newValue ? Colors.tagSkyBlue : backgroundColor }
+       }
+
+       @IBInspectable var coolGreyBlack: Bool {
+           get { return backgroundColor == Colors.coolGreyBlack }
+           set { backgroundColor = newValue ? Colors.coolGreyBlack : backgroundColor }
+       }
+
+       @IBInspectable var coolGrey4: Bool {
+           get { return backgroundColor == Colors.coolGrey4 }
+           set { backgroundColor = newValue ? Colors.coolGrey4 : backgroundColor }
+       }
+
+       @IBInspectable var coolGrey3: Bool {
+           get { return backgroundColor == Colors.coolGrey3 }
+           set { backgroundColor = newValue ? Colors.coolGrey3 : backgroundColor }
+       }
+
+       @IBInspectable var coolGrey2: Bool {
+           get { return backgroundColor == Colors.coolGrey2 }
+           set { backgroundColor = newValue ? Colors.coolGrey2 : backgroundColor }
+       }
+
+       @IBInspectable var coolGrey1: Bool {
+           get { return backgroundColor == Colors.coolGrey1 }
+           set { backgroundColor = newValue ? Colors.coolGrey1 : backgroundColor }
+       }
+
+       @IBInspectable var currentDotColorOfDotIndicator: Bool {
+           get { return backgroundColor == Colors.currentDotColorOfDotIndicator }
+           set { backgroundColor = newValue ? Colors.currentDotColorOfDotIndicator : backgroundColor }
+       }
+
+       @IBInspectable var defaultDotColorOfDotIndicator: Bool {
+           get { return backgroundColor == Colors.defaultDotColorOfDotIndicator }
+           set { backgroundColor = newValue ? Colors.defaultDotColorOfDotIndicator : backgroundColor }
+       }
+}
---
+++ iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewController/NoImageDetailPopupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoImageDetailPopupViewController.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/20.
+//
+
+import UIKit
+
+class NoImageDetailPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
