c3cbb3db4b7a2eef32e2ef9c451105ec5202c4ad
eigen98 <terrantrace@gmail.com>
2023-08-20 14:45:57 +0900

    feat : IntroRepositoryError 열거형 추가 (#181) - 네트워크 오류와 변환 오류를 다루기 위한 새로운 에러 유형 정의 - 각각의 에러 유형에 대해 사용자 친화적인 에러 메시지를 반환하도록 구현 - IntroRepository에서 이 에러 유형을 사용하여 명확한 에러 처리 가능

--- iOS_H3/iOS_H3/Source/Network/IntroRepositoryProtocol.swift
+++ iOS_H3/iOS_H3/Source/Network/IntroRepositoryProtocol.swift
@@ -6,12 +6,25 @@
 //
 
 import Combine
+import Foundation
+
+enum IntroRepositoryError: LocalizedError {
+    case networkError(Error)
+    case conversionError(EstimateElementToEntityError)
+
+    var errorDescription: String? {
+        switch self {
+        case .networkError(let error):
+            return "네트워크 오류: \(error.localizedDescription)"
+        case .conversionError(let error):
+            return "변환 오류: \(error.localizedDescription)"
+        }
+    }
+}
 
 protocol IntroRepositoryProtocol {
 
     typealias APIResult<T> = AnyPublisher<Result<T, Error>, Never>
 
-    func fetchEstimate() -> AnyPublisher<EstimateSummary, Never>
+    func fetchEstimate() -> AnyPublisher<EstimateSummary, Error>
 }
-
-
