54c90b674e06272d36f072599660d33fb0357809
sangyeon <sangyeun33@gmail.com>
2023-08-20 20:01:11 +0900

    feat: CarMakingCategory 정의 (#183)
    - 견적 요약에서 사용할 CarMakingCategory 정의
    - 서버에서 전달받은 category string으로 초기화 가능
    - 디폴트, 색상, 추가 옵션 3가지 경우 존재

---
+++ iOS_H3/iOS_H3/Source/CarMakingCategory.swift
@@ -0,0 +1,14 @@
+//
+//  EstimateSummarySection.swift
+//  iOS_H3_UI
+//
+//  Created by  sangyeon on 2023/08/20.
+//
+
+import Foundation
+
+enum CarMakingCategory: String, CaseIterable {
+    case defaultInfo = "car"
+    case colorInfo = "color"
+    case optionInfo = "additional-option"
+}
