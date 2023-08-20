7fc721b7e1201be9b7e311c80c95de00bf212ebd
eigen98 <terrantrace@gmail.com>
2023-08-20 14:18:25 +0900

    feat : 예상 견적 데이터에 대한 에러 처리 추가(#181) - 모든 프로퍼티가 존재해야 함을 확인하도록 에러 처리를 추가 - 가격이 음수가 아닌지 확인하는 에러 처리를 구현 - EstimateElementData에서 필요한 모든 프로퍼티의 유효성 검사를 수행

--- iOS_H3/DTO/CarInfo/EstimateElementData.swift
+++ iOS_H3/DTO/CarInfo/EstimateElementData.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
+enum EstimateElementToEntityError: LocalizedError {
+    case missingCategory
+    case missingKey
+    case missingValue
+    case negativePrice
+
+    var errorDescription: String? {
+        switch self {
+        case .missingCategory:
+            return "카테고리 값이 없습니다."
+        case .missingKey:
+            return "키 값이 없습니다."
+        case .missingValue:
+            return "타입 값이 없습니다."
+        case .negativePrice:
+            return "가격 값이 음수일 수 없습니다."
+        }
+    }
+}
+
 struct EstimateElementData: Decodable {
     let category: String?
     let key: String?
@@ -15,10 +35,23 @@ struct EstimateElementData: Decodable {
 }
 
 extension EstimateElementData {
-    func toDomain() -> EstimateSummaryElement {
-        return EstimateSummaryElement(stepName: self.key ?? "",
-                                      selectedOption: self.value ?? "",
-                                      category: self.category ?? "",
-                                      price: self.price ?? 0)
+    func toDomain() throws -> EstimateSummaryElement {
+        guard let category = self.category else {
+            throw EstimateElementToEntityError.missingCategory
+        }
+        guard let key = self.key else {
+            throw EstimateElementToEntityError.missingKey
+        }
+        guard let value = self.value else {
+            throw EstimateElementToEntityError.missingValue
+        }
+        guard let price = self.price, price >= 0 else {
+            throw EstimateElementToEntityError.negativePrice
+        }
+
+        return EstimateSummaryElement(stepName: key,
+                                      selectedOption: value,
+                                      category: category,
+                                      price: price)
     }
 }
