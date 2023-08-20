1ea91e5fab7a5c99473bb25cd51f9154a9602170
eigen98 <terrantrace@gmail.com>
2023-08-21 08:25:07 +0900

    feat: PagingControlView 및 PagingControlViewDelegate 프로토콜 초기 구현(#184) - 이전, 다음 버튼 및 페이지 컨트롤 기능을 포함하며, 사용자가 페이지를 네비게이트 할 수 있게 합니다. - 적절한 제약 조건과 제스처 인식기를 사용하여 레이아웃과 상호 작용을 설정하였습니다. - PagingControlViewDelegate 프로토콜을 통해 이전, 다음 버튼 탭 액션을 처리할 수 있도록 하였습니다. - 뷰에 대한 모든 세부 요소 및 기능을 encapsulate하여, 페이징 컨트롤을 쉽게 사용할 수 있게 하였습니다.

--- iOS_H3/iOS_H3_UI/PagingControlView/PagingControlView.swift
+++ iOS_H3/iOS_H3_UI/PagingControlView/PagingControlView.swift
@@ -5,4 +5,181 @@
 //  Created by KoJeongMin  on 2023/08/20.
 //
 
-import Foundation
+import UIKit
+
+protocol PagingControlViewDelegate: AnyObject {
+    func didTapPreviousButton()
+    func didTapNextButton()
+}
+
+class PagingControlView: UIView {
+    
+    // MARK: - UI properties
+    
+    private let previousButtonView: UIView = {
+        let view = UIView()
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    private let nextButtonView: UIView = {
+        let view = UIView()
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    private let previousLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이전"
+        label.textColor = Colors.coolGrey3
+        return label
+    }()
+    
+    private let nextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "다음"
+        label.textColor = Colors.coolGrey3
+        return label
+    }()
+    
+    private let previousImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.left")
+        imageView.tintColor = Colors.coolGrey3
+        return imageView
+    }()
+    
+    private let nextImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.tintColor = Colors.coolGrey3
+        return imageView
+    }()
+    // MARK: - Properties
+    weak var delegate: PagingControlViewDelegate?
+ 
+    private let pageControl: UIPageControl = {
+        let control = UIPageControl()
+        return control
+    }()
+
+    private var currentPage: Int = 0 {
+           didSet {
+               pageControl.currentPage = currentPage
+           }
+       }
+
+    private var numberOfPages: Int = 0 {
+        didSet {
+            pageControl.numberOfPages = numberOfPages
+        }
+    }
+
+    // MARK: - Lifecycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+
+    // MARK: - Helpers
+    @objc private func handlePreviousButtonTap() {
+        if currentPage > 0 {
+            currentPage -= 1
+            delegate?.didTapPreviousButton()
+        }
+    }
+
+    @objc private func handleNextButtonTap() {
+        if currentPage < numberOfPages - 1 {
+            currentPage += 1
+            delegate?.didTapNextButton()
+        }
+    }
+
+    func setNumberOfPages(_ count: Int) {
+        numberOfPages = count
+    }
+
+    func setCurrentPage(_ page: Int) {
+        currentPage = page
+    }
+}
+
+extension PagingControlView{
+    private func setupViews() {
+        addSubViews()
+        setupPageControl()
+        setupGestures()
+        setupConstraints()
+    }
+
+    private func addSubViews() {
+        addSubview(previousButtonView)
+        addSubview(nextButtonView)
+        
+        previousButtonView.addSubview(previousLabel)
+        previousButtonView.addSubview(previousImageView)
+        nextButtonView.addSubview(nextLabel)
+        nextButtonView.addSubview(nextImageView)
+    }
+
+    private func setupPageControl() {
+        addSubview(pageControl)
+    }
+
+    private func setupGestures() {
+        let previousTapGesture = UITapGestureRecognizer(target: self, action: #selector(handlePreviousButtonTap))
+        previousButtonView.addGestureRecognizer(previousTapGesture)
+        
+        let nextTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleNextButtonTap))
+        nextButtonView.addGestureRecognizer(nextTapGesture)
+    }
+
+    private func setupConstraints() {
+        disableAutoresizingMasks()
+        
+        NSLayoutConstraint.activate([
+            previousButtonView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            previousButtonView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            previousButtonView.widthAnchor.constraint(equalToConstant: 80),
+            previousButtonView.heightAnchor.constraint(equalToConstant: 40),
+            
+            nextButtonView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            nextButtonView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            nextButtonView.widthAnchor.constraint(equalToConstant: 80),
+            nextButtonView.heightAnchor.constraint(equalToConstant: 40),
+            
+            pageControl.centerXAnchor.constraint(equalTo: centerXAnchor),
+            pageControl.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            previousImageView.leadingAnchor.constraint(equalTo: previousButtonView.leadingAnchor),
+            previousImageView.centerYAnchor.constraint(equalTo: previousButtonView.centerYAnchor),
+            
+            nextImageView.trailingAnchor.constraint(equalTo: nextButtonView.trailingAnchor, constant: 0),
+            nextImageView.centerYAnchor.constraint(equalTo: nextButtonView.centerYAnchor),
+            
+            previousLabel.leadingAnchor.constraint(equalTo: previousImageView.trailingAnchor, constant: 5),
+            previousLabel.centerYAnchor.constraint(equalTo: previousButtonView.centerYAnchor),
+            
+            nextLabel.trailingAnchor.constraint(equalTo: nextImageView.leadingAnchor),
+            nextLabel.centerYAnchor.constraint(equalTo: nextButtonView.centerYAnchor)
+        ])
+    }
+
+    private func disableAutoresizingMasks() {
+        previousButtonView.translatesAutoresizingMaskIntoConstraints = false
+        nextButtonView.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        previousLabel.translatesAutoresizingMaskIntoConstraints = false
+        nextLabel.translatesAutoresizingMaskIntoConstraints = false
+        previousImageView.translatesAutoresizingMaskIntoConstraints = false
+        nextImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
