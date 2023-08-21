7832596654e946e6dfd3a780137158af54c65925
eigen98 <terrantrace@gmail.com>
2023-08-21 09:59:45 +0900

    feat : PagingController 리팩토링(#184)

--- iOS_H3/iOS_H3_UI/PagingControlView/PagingControlView.swift
+++ iOS_H3/iOS_H3_UI/PagingControlView/PagingControlView.swift
@@ -13,42 +13,42 @@ protocol PagingControlViewDelegate: AnyObject {
 }
 
 class PagingControlView: UIView {
-    
+
     // MARK: - UI properties
-    
+
     private let previousButtonView: UIView = {
         let view = UIView()
         view.isUserInteractionEnabled = true
         return view
     }()
-    
+
     private let nextButtonView: UIView = {
         let view = UIView()
         view.isUserInteractionEnabled = true
         return view
     }()
-    
+
     private let previousLabel: UILabel = {
         let label = UILabel()
         label.text = "이전"
         label.textColor = Colors.coolGrey3
         return label
     }()
-    
+
     private let nextLabel: UILabel = {
         let label = UILabel()
         label.text = "다음"
         label.textColor = Colors.coolGrey3
         return label
     }()
-    
+
     private let previousImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "chevron.left")
         imageView.tintColor = Colors.coolGrey3
         return imageView
     }()
-    
+
     private let nextImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "chevron.right")
@@ -57,26 +57,35 @@ class PagingControlView: UIView {
     }()
     // MARK: - Properties
     weak var delegate: PagingControlViewDelegate?
- 
+
     private let pageControl: UIPageControl = {
         let control = UIPageControl()
         return control
     }()
 
-    private var currentPage: Int = 0 {
-           didSet {
-               pageControl.currentPage = currentPage
-           }
-       }
-
-    private var numberOfPages: Int = 0 {
+    var currentPage: Int {
+        return _currentPage
+    }
+    var numberOfPages: Int {
+        return _numberOfPages
+    }
+    
+    private var _currentPage: Int = 0 {
         didSet {
-            pageControl.numberOfPages = numberOfPages
+            pageControl.currentPage = _currentPage
         }
     }
+    
+    private var _numberOfPages: Int = 0 {
+        didSet {
+            pageControl.numberOfPages = _numberOfPages
+        }
+    }
+    
+    
 
     // MARK: - Lifecycles
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
@@ -90,28 +99,28 @@ class PagingControlView: UIView {
     // MARK: - Helpers
     @objc private func handlePreviousButtonTap() {
         if currentPage > 0 {
-            currentPage -= 1
+            setCurrentPage(currentPage - 1)
             delegate?.didTapPreviousButton()
         }
     }
 
     @objc private func handleNextButtonTap() {
         if currentPage < numberOfPages - 1 {
-            currentPage += 1
+            setCurrentPage(currentPage + 1)
             delegate?.didTapNextButton()
         }
     }
 
-    func setNumberOfPages(_ count: Int) {
-        numberOfPages = count
+    func setCurrentPage(_ page: Int) {
+        _currentPage = page
     }
 
-    func setCurrentPage(_ page: Int) {
-        currentPage = page
+    func setNumberOfPages(_ count: Int) {
+        _numberOfPages = count
     }
 }
 
-extension PagingControlView{
+extension PagingControlView {
     private func setupViews() {
         addSubViews()
         setupPageControl()
@@ -122,7 +131,7 @@ extension PagingControlView{
     private func addSubViews() {
         addSubview(previousButtonView)
         addSubview(nextButtonView)
-        
+
         previousButtonView.addSubview(previousLabel)
         previousButtonView.addSubview(previousImageView)
         nextButtonView.addSubview(nextLabel)
@@ -136,37 +145,37 @@ extension PagingControlView{
     private func setupGestures() {
         let previousTapGesture = UITapGestureRecognizer(target: self, action: #selector(handlePreviousButtonTap))
         previousButtonView.addGestureRecognizer(previousTapGesture)
-        
+
         let nextTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleNextButtonTap))
         nextButtonView.addGestureRecognizer(nextTapGesture)
     }
 
     private func setupConstraints() {
         disableAutoresizingMasks()
-        
+
         NSLayoutConstraint.activate([
             previousButtonView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             previousButtonView.centerYAnchor.constraint(equalTo: centerYAnchor),
             previousButtonView.widthAnchor.constraint(equalToConstant: 80),
             previousButtonView.heightAnchor.constraint(equalToConstant: 40),
-            
+
             nextButtonView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             nextButtonView.centerYAnchor.constraint(equalTo: centerYAnchor),
             nextButtonView.widthAnchor.constraint(equalToConstant: 80),
             nextButtonView.heightAnchor.constraint(equalToConstant: 40),
-            
+
             pageControl.centerXAnchor.constraint(equalTo: centerXAnchor),
             pageControl.centerYAnchor.constraint(equalTo: centerYAnchor),
-            
+
             previousImageView.leadingAnchor.constraint(equalTo: previousButtonView.leadingAnchor),
             previousImageView.centerYAnchor.constraint(equalTo: previousButtonView.centerYAnchor),
-            
+
             nextImageView.trailingAnchor.constraint(equalTo: nextButtonView.trailingAnchor, constant: 0),
             nextImageView.centerYAnchor.constraint(equalTo: nextButtonView.centerYAnchor),
-            
+
             previousLabel.leadingAnchor.constraint(equalTo: previousImageView.trailingAnchor, constant: 5),
             previousLabel.centerYAnchor.constraint(equalTo: previousButtonView.centerYAnchor),
-            
+
             nextLabel.trailingAnchor.constraint(equalTo: nextImageView.leadingAnchor),
             nextLabel.centerYAnchor.constraint(equalTo: nextButtonView.centerYAnchor)
         ])
