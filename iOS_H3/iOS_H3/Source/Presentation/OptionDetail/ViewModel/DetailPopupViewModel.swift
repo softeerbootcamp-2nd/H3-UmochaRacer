737cf1b742dbe837c5ad7b5f199a9cd177e0d18f
eigen98 <terrantrace@gmail.com>
2023-08-21 09:59:18 +0900

    feat : 자세히보기 뷰컨트롤러의 ViewModel 정의(#184)

--- iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewModel/DetailPopupViewModel.swift
+++ iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewModel/DetailPopupViewModel.swift
@@ -5,4 +5,75 @@
 //  Created by KoJeongMin  on 2023/08/21.
 //
 
+import Combine
 import Foundation
+
+final class DetailPopupViewModel {
+
+    // MARK: - Input
+    struct Input {
+        var viewDidLoad: PassthroughSubject<Void, Never>
+        var pageDidChange: CurrentValueSubject<Int, Never>
+        var isImageDetail: Bool
+    }
+
+    // MARK: - Output
+    struct Output {
+        var title = CurrentValueSubject<String, Never>("")
+        var subTitle = CurrentValueSubject<String, Never>("")
+        var description = CurrentValueSubject<String, Never>("")
+        var additionalInfo = CurrentValueSubject<String, Never>("")
+        var pageCount = CurrentValueSubject<Int, Never>(0)
+        var imageURL = CurrentValueSubject<URL?, Never>(nil)
+    }
+
+    // MARK: - Properties
+    private var cancellables = Set<AnyCancellable>()
+    private let usecase: DetailPopupUsecaseProtocol
+
+    // MARK: - Lifecycles
+    init(usecase: DetailPopupUsecaseProtocol? = MockDetailPopupUsecase() ) {
+        self.usecase = usecase!
+    }
+
+    // MARK: - Helpers
+    func transform(_ input: Input) -> Output {
+        let output = Output()
+
+        input.viewDidLoad
+            .sink { _ in
+                output.title.send("Title")
+                output.subTitle.send("Subtitle")
+                output.description.send("Description goes here.")
+                output.additionalInfo.send("Additional Info")
+                output.pageCount.send(5) // 예시 카운트
+
+                if input.isImageDetail {
+                    if let url = URL(string: "https://example.com/image.jpg") {
+                        output.imageURL.send(url)
+                    }
+                }
+            }
+            .store(in: &cancellables)
+
+        input.pageDidChange
+            .flatMap { [weak self] page -> AnyPublisher<DetailInfo, Never> in
+                guard let self = self else {
+                    return Just(DetailInfo(title: "", subTitle: "", description: "", additionalInfo: "", imageURL: nil))
+                        .eraseToAnyPublisher()
+                }
+                return self.usecase.fetchDetailInfo(forPage: page)
+            }
+            .sink { detailInfo in
+                output.title.send(detailInfo.title)
+                output.subTitle.send(detailInfo.subTitle)
+                output.description.send(detailInfo.description)
+                output.additionalInfo.send(detailInfo.additionalInfo)
+                output.imageURL.send(detailInfo.imageURL)
+                print("\(detailInfo.title) 페이지")
+            }
+            .store(in: &cancellables)
+
+        return output
+    }
+}
