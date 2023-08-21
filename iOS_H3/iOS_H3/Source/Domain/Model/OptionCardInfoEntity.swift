7edcc206aa883a13a804f21cf504e561be2e57dd
sangyeon <sangyeun33@gmail.com>
2023-08-21 17:12:01 +0900

    feat: Domain의 OptionCardInfo 모델을 Entity로 수정하고 Presentation 영역에  OptionCardInfo 구현 (#191)
    - OptionCardInfo는 프레젠테이션 레이어, OptionCardInfoEntity는 도메인 레이어
    - OptionCardInfo의 title과 subtitle의 타입을 URString으로 해주기 위함

---
+++ iOS_H3/iOS_H3/Source/Domain/Model/OptionCardInfoEntity.swift
@@ -0,0 +1,39 @@
+//
+//  OptionCardInfoEntity.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/07.
+//
+
+import Foundation
+
+struct OptionCardInfoEntity {
+    let title: String
+    let subTitle: String
+    let priceString: String     // 예시) "+ 100,000원"
+    let bannerImageURL: URL?    // 옵션 카드 상단 큰 이미지
+    let iconImageURL: URL?          // 내장 색상 옵션에만 존재하는 이미지
+    let color: URColor?
+    let hasMoreInfo: Bool       // 자세히보기 여부
+    var isSelected: Bool
+
+    init(
+        title: String,
+        subTitle: String,
+        priceString: String,
+        bannerImageURL: URL? = nil,
+        iconImageURL: URL? = nil,
+        color: URColor? = nil,
+        hasMoreInfo: Bool = false,
+        isSelected: Bool = false
+    ) {
+        self.title = title
+        self.subTitle = subTitle
+        self.priceString = priceString
+        self.bannerImageURL = bannerImageURL
+        self.iconImageURL = iconImageURL
+        self.color = color
+        self.hasMoreInfo = hasMoreInfo
+        self.isSelected = isSelected
+    }
+}
