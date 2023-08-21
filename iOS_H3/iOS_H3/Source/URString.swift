656f082d943a54c5de4a545f7dc7498e1a04cefc
sangyeon <sangyeun33@gmail.com>
2023-08-21 12:09:00 +0900

    feat: URString 정의 (#191)

---
+++ iOS_H3/iOS_H3/Source/URString.swift
@@ -0,0 +1,13 @@
+//
+//  URString.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/21.
+//
+
+import Foundation
+
+struct URString {
+    let fullText: String
+    let cardbRange: [Range<Int>]
+}
