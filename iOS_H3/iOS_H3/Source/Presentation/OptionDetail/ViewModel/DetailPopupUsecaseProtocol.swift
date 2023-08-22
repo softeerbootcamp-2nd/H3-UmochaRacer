0e699f8fd2e1751751cf861b79d3518a34cd66ff
eigen98 <terrantrace@gmail.com>
2023-08-22 11:58:42 +0900

    fix : SwiftLint 경고 수정

--- iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewModel/DetailPopupUsecaseProtocol.swift
+++ iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewModel/DetailPopupUsecaseProtocol.swift
@@ -12,22 +12,32 @@ protocol DetailPopupUsecaseProtocol {
     func fetchDetailInfo(forPage page: Int) -> AnyPublisher<DetailInfo, Never>
 }
 
-class MockDetailPopupUsecase : DetailPopupUsecaseProtocol{
+class MockDetailPopupUsecase: DetailPopupUsecaseProtocol {
     func fetchDetailInfo(forPage page: Int) -> AnyPublisher<DetailInfo, Never> {
         let detailInfo: DetailInfo
         switch page {
         case 0:
-            detailInfo = DetailInfo(title: "Title Page 0", subTitle: "Subtitle Page 0", description: "Description for page 0", additionalInfo: "Additional Info for page 0", imageURL: URL(string: "https://example.com/image0.jpg"))
+            detailInfo = DetailInfo(title: "Title Page 0",
+                                    subTitle: "Subtitle Page 0",
+                                    description: "Description`",
+                                    additionalInfo: "AB",
+                                    imageURL: URL(string: "https://example.com/image0.jpg"))
         case 1:
-            detailInfo = DetailInfo(title: "Title Page 1", subTitle: "Subtitle Page 1", description: "Description for page 1", additionalInfo: "Additional Info for page 1", imageURL: URL(string: "https://example.com/image1.jpg"))
+            detailInfo = DetailInfo(title: "Title Page 1",
+                                    subTitle: "Subtitle Page 1",
+                                    description: "Description2",
+                                    additionalInfo: "B",
+                                    imageURL: URL(string: "https://example.com/image1.jpg"))
         case 2:
-            detailInfo = DetailInfo(title: "Title Page 2", subTitle: "Subtitle Page 2", description: "Description for page 2", additionalInfo: "Additional Info for page 2", imageURL: URL(string: "https://example.com/image2.jpg"))
+            detailInfo = DetailInfo(title: "Title Page 2",
+                                    subTitle: "Subtitle Page 2",
+                                    description: "Description3",
+                                    additionalInfo: "B",
+                                    imageURL: URL(string: "https://example.com/image2.jpg"))
         default:
             detailInfo = DetailInfo(title: "", subTitle: "", description: "", additionalInfo: "", imageURL: nil)
         }
-        
+
         return Just(detailInfo).eraseToAnyPublisher()
     }
-    
-    
 }
