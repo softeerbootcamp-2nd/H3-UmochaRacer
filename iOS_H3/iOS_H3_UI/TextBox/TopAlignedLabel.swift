122887206399ebd48d6eb58bb89e8428e326c084
eigen98 <terrantrace@gmail.com>
2023-08-22 16:44:30 +0900

    feat : 커스텀 상단 정렬 기능 확장 UILabel 서브클래스 구현(#207) 1. TopAlignedLabel이라는 커스텀 레이블 뷰 클래스를 추가하였습니다. 2. 이 클래스는 뷰 내에서 텍스트 레이블이 상단에 정렬되도록 하는 기능을 제공합니다. 3. 텍스트, 텍스트 색상, 폰트를 외부에서 설정할 수 있도록 메서드를 추가하였습니다.

--- iOS_H3/iOS_H3_UI/TextBox/TopAlignedLabel.swift
+++ iOS_H3/iOS_H3_UI/TextBox/TopAlignedLabel.swift
@@ -5,4 +5,46 @@
 //  Created by KoJeongMin  on 2023/08/22.
 //
 
-import Foundation
+import UIKit
+
+class TopAlignedLabel: UIView {
+    private var label: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor),
+            label.topAnchor.constraint(equalTo: topAnchor)
+        ])
+        label.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+
+    func setText(_ text: String) {
+        label.text = text
+    }
+    
+    func setTextColor(_ color: UIColor) {
+        label.textColor = color
+    }
+    
+    func setFont(_ font: UIFont) {
+        label.font = font
+    }
+}
