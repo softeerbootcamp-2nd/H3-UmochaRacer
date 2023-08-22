28d0f18224db5b94ba2610127ce52edc0fdd3f60
sangyeon <sangyeun33@gmail.com>
2023-08-22 17:27:10 +0900

    chore: SceneDelegate - lint 경고 수정

--- iOS_H3/iOS_H3/Source/App/SceneDelegate.swift
+++ iOS_H3/iOS_H3/Source/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        
+
         let networkService = NetworkService()
         let carInfoRepository = CarInfoRepository(networkService: networkService)
         let introRepository = IntroRepository(networkService: networkService)
