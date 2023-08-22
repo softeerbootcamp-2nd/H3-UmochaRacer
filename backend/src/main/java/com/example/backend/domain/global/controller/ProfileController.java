d227027a857922b0b6e38034f183bf2df9fdd145
Park Seonmi <sm000117@naver.com>
2023-08-22 14:50:58 +0900

    fix: ProfileController에 request mapping 추가

--- backend/src/main/java/com/example/backend/domain/global/controller/ProfileController.java
+++ backend/src/main/java/com/example/backend/domain/global/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.example.backend.domain.global.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class ProfileController {
     private final Environment env;
 
