45c49605faf7fcc5ebc796a6d08f8c49b37f971a
sangyeon <sangyeun33@gmail.com>
2023-08-23 02:58:22 +0900

    chore: CarInfoMockRepository - mock 데이터에 id 프로퍼티 추가

--- iOS_H3/iOS_H3/Source/Data/Repository/CarInfoMockRepository.swift
+++ iOS_H3/iOS_H3/Source/Data/Repository/CarInfoMockRepository.swift
@@ -15,7 +15,7 @@ final class CarInfoMockRepository: CarInfoRepositoryProtocol {
         step: CarMakingStep
     ) -> AnyPublisher<CarMakingStepInfoEntity, CarInfoRepositoryError> {
         let stepIndex = step.rawValue
-        let optionInfoArray = CarMakingMockData.mockOption[stepIndex]
+        let optionInfoArray = CarMakingMockRepositoryData.mockOption[stepIndex]
         let stepInfo = CarMakingStepInfoEntity(step: step, optionCardInfoArray: optionInfoArray)
         return Just(stepInfo).setFailureType(to: CarInfoRepositoryError.self).eraseToAnyPublisher()
     }
@@ -69,84 +69,123 @@ final class CarInfoMockRepository: CarInfoRepositoryProtocol {
     }
 }
 
-struct CarMakingMockData {
+struct CarMakingMockRepositoryData {
     static let mockURL = [
         "https://cdn.autotribune.co.kr/news/photo/202101/4849_30727_3533.jpg",
-         // swiftlint:disable:next line_length
-         "https://i.namu.wiki/i/cBMX6XiTLltPPIawbb2zfP5Oy5RW9JybY0E5ZQ62oUYdppA2t54xDjiST7xfLe_2dL4pGN9VsBVknq4H-SYA2A.webp",
-         "https://cdn.epnc.co.kr/news/photo/201804/79474_70575_4841.jpg",
-         "https://pimg.daara.co.kr/kidd/photo/2021/01/08/thumbs/thumb_520390_1610089982_79.jpg",
+        // swiftlint:disable:next line_length
+        "https://i.namu.wiki/i/cBMX6XiTLltPPIawbb2zfP5Oy5RW9JybY0E5ZQ62oUYdppA2t54xDjiST7xfLe_2dL4pGN9VsBVknq4H-SYA2A.webp",
+        "https://cdn.epnc.co.kr/news/photo/201804/79474_70575_4841.jpg",
+        "https://pimg.daara.co.kr/kidd/photo/2021/01/08/thumbs/thumb_520390_1610089982_79.jpg",
         "https://pimg.daara.co.kr/kidd/photo/2021/01/08/thumbs/thumb_520390_1610089971_11.jpg",
-         "https://itimg.chosun.com/sitedata/image/202112/03/2021120301496_0.jpg"
+        "https://itimg.chosun.com/sitedata/image/202112/03/2021120301496_0.jpg"
     ].compactMap { URL(string: $0) }
-
+    
     static var mockOption = [
-            [OptionCardInfoEntity.init(title: "디젤 2.2",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[0]),
-             OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 37%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[1])],
-            [OptionCardInfoEntity.init(title: "2WD",
-                                 subTitle: "구매자의 72%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[2]),
-             OptionCardInfoEntity.init(title: "4WD",
-                                 subTitle: "구매자의 82%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[0])],
-            [OptionCardInfoEntity.init(title: "7인승",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[1]),
-             OptionCardInfoEntity.init(title: "8인승",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[3])],
-            [OptionCardInfoEntity.init(title: "크리미 화이트 펄",
-                                 subTitle: "구매자의 88%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[4],
-                                 color: URColor(red: 0, green: 0, blue: 0)),
-             OptionCardInfoEntity.init(title: "LK-99 3.8",
-                                 subTitle: "구매자의 12%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[0])],
-            [OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[5],
-                                 // swiftlint:disable:next line_length
-                                 iconImageURL: URL(string: "https://github.com/sangyeon3/kakao_pathfinder_assignment/assets/68235938/fe0d0580-f5b2-47fe-80ff-bab37e6f4815")),
-             OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[1])],
-            [OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[0]),
-             OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[4])],
-            [OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[3]),
-             OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 3,456,789원",
-                                 bannerImageURL: mockURL[0])],
-            [OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 0 원",
-                                 bannerImageURL: mockURL[1]),
-             OptionCardInfoEntity.init(title: "가솔린 3.8",
-                                 subTitle: "구매자의 63%가 선택한",
-                                 priceString: "+ 0 원",
-                                 bannerImageURL: mockURL[2])]
+        [OptionCardInfoEntity.init(
+            id: 0,
+            title: "디젤 2.2",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[0],
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 1,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 37%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[1])],
+        [OptionCardInfoEntity.init(
+            id: 2,
+            title: "2WD",
+            subTitle: "구매자의 72%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[2],
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 3,
+            title: "4WD",
+            subTitle: "구매자의 82%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[0])],
+        [OptionCardInfoEntity.init(
+            id: 4,
+            title: "7인승",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[1],
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 5,
+            title: "8인승",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[3])],
+        [OptionCardInfoEntity.init(
+            id: 6,
+            title: "크리미 화이트 펄",
+            subTitle: "구매자의 88%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[4],
+            color: URColor(red: 0, green: 0, blue: 0),
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 7,
+            title: "LK-99 3.8",
+            subTitle: "구매자의 12%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[0])],
+        [OptionCardInfoEntity.init(
+            id: 8,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[5],
+            // swiftlint:disable:next line_length
+            iconImageURL: URL(string: "https://github.com/sangyeon3/kakao_pathfinder_assignment/assets/68235938/fe0d0580-f5b2-47fe-80ff-bab37e6f4815"),
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 9,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[1])],
+        [OptionCardInfoEntity.init(
+            id: 10,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[0],
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 11,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[4])],
+        [OptionCardInfoEntity.init(
+            id: 12,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[3],
+            isSelected: true),
+         OptionCardInfoEntity.init(
+            id: 13,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 3,456,789원",
+            bannerImageURL: mockURL[0])],
+        [OptionCardInfoEntity.init(
+            id: 14,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 0 원",
+            bannerImageURL: mockURL[1]),
+         OptionCardInfoEntity.init(
+            id: 15,
+            title: "가솔린 3.8",
+            subTitle: "구매자의 63%가 선택한",
+            priceString: "+ 0 원",
+            bannerImageURL: mockURL[2])]
     ]
 }
