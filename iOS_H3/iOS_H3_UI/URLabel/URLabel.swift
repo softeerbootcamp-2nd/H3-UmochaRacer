eeb4ec602951cd241f74e4864a2e2297da2a9079
eigen98 <terrantrace@gmail.com>
2023-08-22 10:47:32 +0900

    feat : URLabel 클래스에 선택 상태 관리 추가(#196) 1. RLabel 클래스에 선택된 범위를 추적하고 관리하기 위해 selectedRange 속성을 추가 2. 단어를 선택하거나 선택을 해제할 때 적절한 효과를 적용

--- iOS_H3/iOS_H3_UI/URLabel/URLabel.swift
+++ iOS_H3/iOS_H3_UI/URLabel/URLabel.swift
@@ -9,18 +9,25 @@ import UIKit
 
 class URLabel: UILabel {
     var discriptionLabel: DictionaryString?
+    private var selectedRange: NSRange?
 
     func setURString(_ urString: DictionaryString) {
         let attributedString = NSMutableAttributedString(string: urString.fullText)
 
         for range in urString.cardbRange {
             let nsRange = NSRange(range, in: urString.fullText)
-
-            attributedString.addAttribute(.underlineStyle,
-                                          value: NSUnderlineStyle.single.rawValue,
-                                          range: nsRange)
+            if nsRange == selectedRange {
+                highlightForDictionarySelection(range: nsRange, linkAction: {
+                    self.selectedRange = nil
+                    self.setURString(urString)
+                })
+            } else {
+                highlightForDictionaryActivation(range: nsRange, linkAction: {
+                    self.selectedRange = nsRange
+                    self.setURString(urString)
+                })
+            }
         }
-
         self.attributedText = attributedString
     }
 }
