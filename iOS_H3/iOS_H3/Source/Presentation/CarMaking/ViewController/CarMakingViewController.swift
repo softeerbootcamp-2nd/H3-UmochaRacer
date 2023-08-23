1620f490b28e285a041899b27faadb78d615043c
eigen98 <terrantrace@gmail.com>
2023-08-23 14:35:45 +0900

    feat : 백카사전 버튼 클릭 이벤트 구현(#207) 1. TextEffectManager를 통해 백카사전 하이라이트를 적용할 수 있는 로직을 추가합니다. 2. 옵션 버튼을 클릭할 경우 백카사전 하이라이트를 초기화합니다.

--- iOS_H3/iOS_H3/Source/Presentation/CarMaking/ViewController/CarMakingViewController.swift
+++ iOS_H3/iOS_H3/Source/Presentation/CarMaking/ViewController/CarMakingViewController.swift
@@ -29,6 +29,8 @@ final class CarMakingViewController: UIViewController {
 
     private let viewModel: CarMakingViewModel
 
+    private let textEffectManager = TextEffectManager()
+
     private let viewDidLoadSubject = PassthroughSubject<Void, Never>()
 
     private let stepDidChanged = CurrentValueSubject<CarMakingStep, Never>(.powertrain)
@@ -36,6 +38,8 @@ final class CarMakingViewController: UIViewController {
     private let optionDidSelected = PassthroughSubject<(step: CarMakingStep, optionIndex: Int), Never>()
 
     private let optionCategoryDidChanged = PassthroughSubject<OptionCategoryType, Never>()
+
+    private var dictionaryButtonPressed = PassthroughSubject<Void, Never>()
 
     private var cancellables = Set<AnyCancellable>()
 
@@ -79,7 +83,8 @@ extension CarMakingViewController {
             viewDidLoad: viewDidLoadSubject,
             carMakingStepDidChanged: stepDidChanged,
             optionDidSelected: optionDidSelected,
-            optionCategoryDidChanged: optionCategoryDidChanged
+            optionCategoryDidChanged: optionCategoryDidChanged,
+            dictionaryButtonPressed: dictionaryButtonPressed
         )
         let output = viewModel.transform(input)
 
@@ -93,6 +98,7 @@ extension CarMakingViewController {
         output.currentStepInfo
             .receive(on: DispatchQueue.main)
             .sink { [weak self] info in
+                self?.titleBar.resetDictionary()
                 self?.updateCurrentStepInfo(with: info)
             }
             .store(in: &cancellables)
@@ -100,6 +106,12 @@ extension CarMakingViewController {
         output.optionInfoDidUpdated
             .sink { [weak self] optionInfo in
                 self?.carMakingContentView.updateOptionCard(with: optionInfo)
+                if let view = self?.view {
+                    if output.isDictionaryFeatureEnabled.value {
+                        self?.textEffectManager.applyEffect(false, on: view)
+                        self?.textEffectManager.applyEffect(true, on: view)
+                    }
+                }
             }
             .store(in: &cancellables)
 
@@ -108,10 +120,17 @@ extension CarMakingViewController {
                 self?.showIndicator(showIndicator)
             }
             .store(in: &cancellables)
+
+        output.isDictionaryFeatureEnabled
+            .sink { [weak self] isEnabled in
+                if let view = self?.view {
+                    self?.textEffectManager.applyEffect(isEnabled, on: view)
+                }
+            }
+            .store(in: &cancellables)
     }
 
     private func updateBottomModalView(with estimateData: EstimateSummary) {
-        // 총 견적금액 계산해서 bottomModalView.updateEstimatePrice(price) 호출
         bottomModalView.updateEstimateSummary(estimateData)
     }
 
@@ -141,7 +160,7 @@ extension CarMakingViewController: OhMyCarSetTitleBarDelegate {
     }
 
     func titleBarDictionaryButtonPressed(_ titleBar: OhMyCarSetTitleBar) {
-        print("[CarMakingViewController]", #function, "백카사전 버튼 클릭 액션 구현 필요")
+        dictionaryButtonPressed.send(())
     }
 
     func titleBarChangeModelButtonPressed(_ titleBar: OhMyCarSetTitleBar) {
