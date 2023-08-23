b9ea0c34e8b5d9864b723f9a06d8a5966338574b
eigen98 <terrantrace@gmail.com>
2023-08-23 18:27:38 +0900

    feat :  DetailRepository 내 파워트레인 상세 정보 조회 기능 구현(#225) - DetailRepository에 fetchPowertrainDetailInfo 메서드 추가 - 네트워크 요청 결과에 따른 오류 처리 및 DetailOptionToEntityError로의 변환 로직 포함

--- iOS_H3/iOS_H3/Source/DetailRepository.swift
+++ iOS_H3/iOS_H3/Source/DetailRepository.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import Combine
+
+class DetailRepository: DetailRepositoryProtocol {
+
+    private let networkService: NetworkServiceProtocol
+
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
+    }
+
+    func fetchPowertrainDetailInfo() -> AnyPublisher<DetailOptionEntity, DetailRepositoryError> {
+        networkService.request(DetailEndPoint.powertrain(id: 1))
+            .flatMap { (result: Result<APIResponse<DetailOptionData>, Error>)
+                -> AnyPublisher<DetailOptionEntity, DetailRepositoryError> in
+                switch result {
+                case .success(let data):
+                    do {
+                        let detailOptionEntity = try data.data.toDomain()
+                        return Just(detailOptionEntity)
+                            .setFailureType(to: DetailRepositoryError.self)
+                            .eraseToAnyPublisher()
+                    } catch let error as DetailOptionToEntityError {
+                        return Fail(outputType: DetailOptionEntity.self,
+                                    failure: DetailRepositoryError.conversionError(error))
+                            .eraseToAnyPublisher()
+                    } catch {
+                        return Fail(outputType: DetailOptionEntity.self,
+                                    failure: DetailRepositoryError.networkError(error))
+                            .eraseToAnyPublisher()
+                    }
+                case .failure(let error):
+                    return Fail(outputType: DetailOptionEntity.self,
+                                failure: DetailRepositoryError.networkError(error))
+                        .eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+}
