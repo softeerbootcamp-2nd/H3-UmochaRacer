dfd954db049cb8bd3213ff4c109fcad0cc275ca7
sangyeon <sangyeun33@gmail.com>
2023-08-23 13:01:18 +0900

    feat: CarInfoRepository - fetchFeedbackComment 메소드 구현 (#197)
    내차만들기 단계에 따라 CommentEndpoint를 생성하고 서버에 코멘트 데이터 요청 후 반환

--- iOS_H3/DTO/CarInfo/CarInfoRepository.swift
+++ iOS_H3/DTO/CarInfo/CarInfoRepository.swift
@@ -94,14 +94,45 @@ final class CarInfoRepository: CarInfoRepositoryProtocol {
         fetchCarMakingStepInfo(for: CarInfoEndpoint.singleExteriorColor(optionId: optionId), step: .externalColor)
     }
 
-    func fetchFeedbackComment(
-        step: CarMakingStep,
-        optionID: Int
-    ) -> AnyPublisher<FeedbackCommentEntity, CarInfoRepositoryError> {
-        Just(
-            FeedbackCommentEntity(comment: "temp")
-        )
-        .setFailureType(to: CarInfoRepositoryError.self)
-        .eraseToAnyPublisher()
+    func fetchFeedbackComment(step: CarMakingStep, optionID: Int) -> AnyPublisher<FeedbackCommentEntity, Error> {
+        typealias ResultType = Result<APIResponse<FeedbackCommentData>, Error>
+
+        guard let endpoint = createCommentEndpoint(step: step, optionID: optionID) else {
+            return Fail(error: CarInfoRepositoryError.notExistCommentEndpoint(step: step)).eraseToAnyPublisher()
+        }
+        return networkService.request(endpoint)
+            .flatMap { (result: ResultType) -> AnyPublisher<FeedbackCommentEntity, Error> in
+                switch result {
+                case .success(let response):
+                    do {
+                        let commentEntity = try response.data.toDomain()
+                        return Just(commentEntity).setFailureType(to: Error.self).eraseToAnyPublisher()
+                    } catch {
+                        return Fail(error: CarInfoRepositoryError.conversionError(error)).eraseToAnyPublisher()
+                    }
+                case .failure(let error):
+                    return Fail(error: CarInfoRepositoryError.networkError(error)).eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private func createCommentEndpoint(step: CarMakingStep, optionID: Int) -> CommentEndpoint? {
+        switch step {
+        case .powertrain:
+            return CommentEndpoint.powertrain(id: optionID)
+        case .driveMethod:
+            return CommentEndpoint.drivingSystem(id: optionID)
+        case .bodyType:
+            return CommentEndpoint.bodyType(id: optionID)
+        case .externalColor:
+            return CommentEndpoint.exteriorColor(id: optionID)
+        case .internalColor:
+            return CommentEndpoint.interiorColor(id: optionID)
+        case .wheelSelection:
+            return CommentEndpoint.wheel(id: optionID)
+        default:
+            return nil
+        }
     }
 }
