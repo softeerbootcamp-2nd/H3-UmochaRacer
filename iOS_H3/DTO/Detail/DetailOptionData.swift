d4790da0d3875f241f4e3091afac4a6e56387655
eigen98 <terrantrace@gmail.com>
2023-08-23 17:54:01 +0900

    feat : DetailOptionData 도메인 엔터티 변환 기능 추가(#225) - DetailOptionToEntityError enum을 추가하여 필수 항목 누락 시 에러 처리 - DetailOptionData에 toDomain() 확장 함수를 구현하여 DetailOptionEntity로 변환 가능 - 해당 기능은 도메인 엔터티 변환 시 데이터의 무결성을 보장하기 위해 구현됨

--- iOS_H3/DTO/Detail/DetailOptionData.swift
+++ iOS_H3/DTO/Detail/DetailOptionData.swift
@@ -7,11 +7,45 @@
 
 import Foundation
 
+enum DetailOptionToEntityError: LocalizedError {
+    case missingTitle
+    case missingDescription
+
+    var errorDescription: String? {
+        switch self {
+        case .missingTitle:
+            return "제목 값이 없습니다."
+        case .missingDescription:
+            return "설명 값이 없습니다."
+        }
+    }
+}
+
 struct DetailOptionData: Decodable {
     let title: String?
     let description: String?
     let info: String?
     let imageSrc: Int?
+}
+
+extension DetailOptionData {
+    func toDomain() throws -> DetailOptionEntity {
+
+        guard let title = self.title else {
+            throw DetailOptionToEntityError.missingTitle
+        }
+
+        guard let description = self.description else {
+            throw DetailOptionToEntityError.missingDescription
+        }
+
+        return DetailOptionEntity(
+            title: title,
+            description: description,
+            info: self.info,
+            imageSrc: self.imageSrc
+        )
+    }
 }
 
 struct DetailOptionEntity {
