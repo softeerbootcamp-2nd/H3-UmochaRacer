0911b495379c88e2b4b649215fb59c305ed46003
jjy0709 <jjy0709@kaist.ac.kr>
2023-08-23 14:41:46 +0900

    fix: 배포 스크립트 오류 수정

--- backend/deploy_files/run_new_was.sh
+++ backend/deploy_files/run_new_was.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+cd /home/ubuntu/app/backend
 IS_GREEN=$(sudo docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
 DEFAULT_CONF="/etc/nginx/nginx.conf"
 
-if [ -z $IS_GREEN  ];
+if [ -z $IS_GREEN  ]
 then # blue라면
 
   echo "### BLUE => GREEN ###"
+  echo "0. 도커 이미지 생성"
+  sudo docker-compose build green
 
   echo "1. green 이미지 가져옴"
   sudo docker-compose pull green # green으로 이미지를 내려받습니다.
@@ -12,16 +17,18 @@ then # blue라면
   echo "2. green 컨테이너 올림"
   sudo docker-compose up -d green # green 컨테이너 실행
 
-  while [ 1 = 1 ]; do
-  echo "3. green health check..."
-  sleep 3
+  while [ 1 = 1 ]
+  do
+    echo "3. green health check..."
+    sleep 3
 
-  REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-            echo "health check success"
-            break ;
-            fi
-  done;
+    REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
+    if [ -n $REQUEST ]
+    then # 서비스 가능하면 health check 중지
+      echo "health check success"
+      break
+    fi
+  done
 
   echo "4. nginx 재가동"
   sudo cp nginx.green.conf /etc/nginx/nginx.conf
@@ -31,6 +38,8 @@ then # blue라면
   sudo docker-compose stop blue
 else
   echo "### GREEN => BLUE ###"
+  echo "0. 도커 이미지 생성"
+  sudo docker-compose build blue
 
   echo "1. blue 이미지 가져옴"
   sudo docker-compose pull blue
@@ -38,16 +47,18 @@ else
   echo "2. blue 컨테이너 올림"
   sudo docker-compose up -d blue
 
-  while [ 1 = 1 ]; do
+  while [ 1 = 1 ]
+    do
     echo "3. blue health check..."
     sleep 3
     REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
 
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+    if [ -n $REQUEST ]
+     then # 서비스 가능하면 health check 중지
       echo "health check success"
-      break ;
+      break
     fi
-  done;
+  done
 
   echo "4. nginx 재가동" 
   sudo cp nginx.blue.conf /etc/nginx/nginx.conf
