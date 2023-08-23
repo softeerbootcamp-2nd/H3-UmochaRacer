9b4cfc0532a589276ec710c4fbb8b1d453cf80d9
eigen98 <terrantrace@gmail.com>
2023-08-23 14:49:28 +0900

    feat : UIView extension에 상위 ViewController 탐색 메서드 추가(#207) - `UIView` extension에 `findViewController()` 메서드를 추가하여, 해당 뷰가 포함된 상위 `UIViewController`를 탐색할 수 있게 하였습니다. - 이 메서드는 뷰 계층구조에서 해당 뷰를 소유하는 뷰 컨트롤러를 찾기 위한 유틸리티로 사용됩니다.

--- iOS_H3/iOS_H3/Source/Extensions/UIView+.swift
+++ iOS_H3/iOS_H3/Source/Extensions/UIView+.swift
@@ -26,4 +26,14 @@ extension UIView {
             }
         }
     }
+
+    func findViewController() -> UIViewController? {
+        if let nextResponder = self.next as? UIViewController {
+            return nextResponder
+        } else if let nextResponder = self.next as? UIView {
+            return nextResponder.findViewController()
+        } else {
+            return nil
+        }
+    }
 }
