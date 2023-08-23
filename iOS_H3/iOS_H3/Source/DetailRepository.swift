34e0e4ac5221c7ebbf11c3e800ef39e74908d8b6
eigen98 <terrantrace@gmail.com>
2023-08-23 20:53:31 +0900

    feat : 차량 옵션 상세 정보를 일괄적으로 불러오기 위한 공통 함수 추가(#225) 모든 차량 옵션들이 동일한 응답 형태를 가지므로, 각각의 엔드포인트에 대해 중복된 코드를 작성하는 대신 공통 함수 'fetchDetailInfo'를 추가하여 코드 중복을 최소화하였습니다. 이 함수는 특정 엔드포인트를 인자로 받아 해당 엔드포인트에 대한 상세 정보를 반환

--- iOS_H3/iOS_H3/Source/DetailRepository.swift
+++ iOS_H3/iOS_H3/Source/DetailRepository.swift
@@ -16,6 +16,36 @@ class DetailRepository: DetailRepositoryProtocol {
         self.networkService = networkService
     }
 
+    private func fetchDetailInfo(endpoint: DetailEndPoint)
+    -> AnyPublisher<[DetailOptionEntity], DetailRepositoryError> {
+            networkService.request(endpoint)
+                .flatMap { (result: Result<APIResponse<[DetailOptionData]>, Error>)
+                    -> AnyPublisher<[DetailOptionEntity], DetailRepositoryError> in
+                    switch result {
+                    case .success(let data):
+                        do {
+                            let detailOptionEntities = try data.data.map { try $0.toDomain() }
+                            return Just(detailOptionEntities)
+                                .setFailureType(to: DetailRepositoryError.self)
+                                .eraseToAnyPublisher()
+                        } catch let error as DetailOptionToEntityError {
+                            return Fail(outputType: [DetailOptionEntity].self,
+                                        failure: DetailRepositoryError.conversionError(error))
+                                .eraseToAnyPublisher()
+                        } catch {
+                            return Fail(outputType: [DetailOptionEntity].self,
+                                        failure: DetailRepositoryError.networkError(error))
+                                .eraseToAnyPublisher()
+                        }
+                    case .failure(let error):
+                        return Fail(outputType: [DetailOptionEntity].self,
+                                    failure: DetailRepositoryError.networkError(error))
+                            .eraseToAnyPublisher()
+                    }
+                }
+                .eraseToAnyPublisher()
+        }
+
     func fetchPowertrainDetailInfo(id: Int) -> AnyPublisher<[DetailOptionEntity], DetailRepositoryError> {
         networkService.request(DetailEndPoint.powertrain(id: id))
             .flatMap { (result: Result<APIResponse<[DetailOptionData]>, Error>)
