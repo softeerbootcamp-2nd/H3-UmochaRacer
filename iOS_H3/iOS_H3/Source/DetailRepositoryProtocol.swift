0731b1a4b9edd8a0e829d82979c9eb3ef32ff130
eigen98 <terrantrace@gmail.com>
2023-08-23 18:00:18 +0900

    feat : DetailRepository 구조 및 오류 처리 기능 추가(#225) - DetailRepositoryError enum을 통해 네트워크와 데이터 변환 관련 오류를 정의 - DetailRepositoryProtocol에서 파워트레인 상세 정보를 가져오는 메서드 정의 - 이 변경사항은 상세 정보 조회 시 오류 처리를 강화하고, 통일된 Repository 패턴을 적용하기 위해 진행됨

--- iOS_H3/iOS_H3/Source/DetailRepositoryProtocol.swift
+++ iOS_H3/iOS_H3/Source/DetailRepositoryProtocol.swift
@@ -5,4 +5,23 @@
 //  Created by KoJeongMin  on 2023/08/23.
 //
 
+import Combine
 import Foundation
+
+enum DetailRepositoryError: LocalizedError {
+    case networkError(Error)
+    case conversionError(DetailOptionToEntityError)
+
+    var errorDescription: String? {
+        switch self {
+        case .networkError(let error):
+            return "[DetailRepositoryError] 네트워크 오류: \(error.localizedDescription)"
+        case .conversionError(let error):
+            return "[DetailRepositoryError] 변환 오류: \(error.localizedDescription)"
+        }
+    }
+}
+
+protocol DetailRepositoryProtocol {
+    func fetchPowertrainDetailInfo() -> AnyPublisher<DetailOptionEntity, DetailRepositoryError>
+}
