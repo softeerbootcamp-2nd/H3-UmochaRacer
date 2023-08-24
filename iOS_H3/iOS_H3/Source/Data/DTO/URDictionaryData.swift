f3cde76822cc5f23011d781ef50f8d2bf608691d
sangyeon <sangyeun33@gmail.com>
2023-08-25 01:35:29 +0900

    feat: URDictionaryDataToEntityError 정의 (#240)

--- iOS_H3/iOS_H3/Source/Data/DTO/URDictionaryData.swift
+++ iOS_H3/iOS_H3/Source/Data/DTO/URDictionaryData.swift
@@ -1,3 +1,26 @@
+//
+//  DictionaryData.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/24.
+//
+
+import Foundation
+
+enum URDictionaryDataToEntityError: LocalizedError {
+    case missingKeyword
+    case missingDescription
+
+    var errorDescription: String? {
+        switch self {
+        case .missingKeyword:
+            return "키워드가 존재하지 않습니다."
+        case .missingDescription:
+            return "키워드에 대한 설명이 존재하지 않습니다."
+        }
+    }
+}
+
 struct URDictionaryData: Decodable {
     let keyword: String?
     let description: String?
