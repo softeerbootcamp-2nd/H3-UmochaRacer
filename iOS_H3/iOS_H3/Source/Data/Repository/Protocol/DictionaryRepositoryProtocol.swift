cac2033ef6dc6724a4d1c216d6b6b69c9ce4eb02
sangyeon <sangyeun33@gmail.com>
2023-08-25 01:52:54 +0900

    feat: DictionaryRepositoryError 정의 (#240)

--- iOS_H3/iOS_H3/Source/Data/Repository/Protocol/DictionaryRepositoryProtocol.swift
+++ iOS_H3/iOS_H3/Source/Data/Repository/Protocol/DictionaryRepositoryProtocol.swift
@@ -8,6 +8,20 @@
 import Foundation
 import Combine
 
+enum DictionaryRepositoryError: LocalizedError {
+    case networkError(Error)
+    case conversionError(Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .networkError(let error):
+            return "[DictionaryRepository] 네트워크 오류: \(error.localizedDescription)"
+        case .conversionError(let error):
+            return "[DictionaryRepository] 변환 오류: \(error.localizedDescription)"
+        }
+    }
+}
+
 protocol DictionaryRepositoryProtocol {
     func fetchDictionaryDescription(for keyword: String) -> AnyPublisher<URDictionaryEntity, Error>
 }
