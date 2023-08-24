d55adf5f5e85809fdf9af8d17f2cef370f68db42
sangyeon <sangyeun33@gmail.com>
2023-08-25 01:53:10 +0900

    feat: DictionaryRepository 구현 (#240)

---
+++ iOS_H3/iOS_H3/Source/Data/Repository/DictionaryRepository.swift
@@ -0,0 +1,40 @@
+//
+//  DictionaryRepository.swift
+//  iOS_H3
+//
+//  Created by  sangyeon on 2023/08/24.
+//
+
+import Foundation
+import Combine
+
+final class DictionaryRepository: DictionaryRepositoryProtocol {
+
+    private let networkService: NetworkServiceProtocol
+
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
+    }
+
+    func fetchDictionaryDescription(for keyword: String) -> AnyPublisher<URDictionaryEntity, Error> {
+        typealias ResultType = Result<APIResponse<URDictionaryData>, Error>
+        typealias ReturnType = AnyPublisher<URDictionaryEntity, Error>
+
+        let endpoint = DictionaryEndpoint.cardb(keyword: keyword)
+        return networkService.request(endpoint)
+            .flatMap { (result: ResultType) -> ReturnType in
+                switch result {
+                case .success(let response):
+                    do {
+                        let entity = try response.data.toDomain()
+                        return Just(entity).setFailureType(to: Error.self).eraseToAnyPublisher()
+                    } catch {
+                        return Fail(error: DictionaryRepositoryError.conversionError(error)).eraseToAnyPublisher()
+                    }
+                case .failure(let error):
+                    return Fail(error: DictionaryRepositoryError.networkError(error)).eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+}
