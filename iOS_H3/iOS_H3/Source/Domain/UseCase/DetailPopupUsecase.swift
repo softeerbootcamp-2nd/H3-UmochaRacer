e74025446b63607a01f3205957fe2af46cd20fb9
eigen98 <terrantrace@gmail.com>
2023-08-25 23:06:06 +0900

    feat : 자세히보기 usecase 구현(#231)

--- iOS_H3/iOS_H3/Source/Domain/UseCase/DetailPopupUsecase.swift
+++ iOS_H3/iOS_H3/Source/Domain/UseCase/DetailPopupUsecase.swift
@@ -5,4 +5,75 @@
 //  Created by KoJeongMin  on 2023/08/25.
 //
 
-import Foundation
+import Combine
+
+final class DetailPopupUsecase: DetailPopupUsecaseProtocol {
+    private let detailRepository: MockDetailRepositoryProtocol
+    private var details: MockDetailOptionEntity = MockDetailOptionEntity(title: "",
+                                                                         description: "",
+                                                                         info: nil,
+                                                                         imageSrc: nil)
+    private var cancellables = Set<AnyCancellable>()
+
+    init(repository: MockDetailRepositoryProtocol = MockDetailRepository(networkService: NetworkService())) {
+        self.detailRepository = repository
+    }
+
+    func fetchAllDetails(step: CarMakingStep, id: Int) -> AnyPublisher<DetailOptionInfo, DetailUsecaseError> {
+        guard let publisher = publisherForStep(step, id: id) else {
+            return Fail<DetailOptionInfo, DetailUsecaseError>(error: DetailUsecaseError.invalidStep)
+                .eraseToAnyPublisher()
+        }
+
+        let errorMappedPublisher: AnyPublisher<MockDetailOptionEntity, DetailUsecaseError> = publisher
+            .mapError { error in
+                return self.convertToDetailUsecaseError(from: error)
+            }
+            .eraseToAnyPublisher()
+
+        let caughtPublisher: AnyPublisher<MockDetailOptionEntity, Never> = errorMappedPublisher
+            .catch { error -> Just<MockDetailOptionEntity> in
+                print("Error: \(error.localizedDescription)")
+                return Just(MockDetailOptionEntity(title: "", description: "", info: nil, imageSrc: nil))
+            }
+            .eraseToAnyPublisher()
+
+        return caughtPublisher.map { $0.toPresentation() }
+            .setFailureType(to: DetailUsecaseError.self)
+            .eraseToAnyPublisher()
+    }
+
+    func fetchAllAdditionalOptions(id: Int) -> AnyPublisher<[DetailOptionInfo], DetailUsecaseError> {
+        detailRepository.fetchAdditionalOptionDetailInfo(id: id)
+            .mapError { self.convertToDetailUsecaseError(from: $0) }
+            .map { $0.map { $0.toPresentation() } }
+            .eraseToAnyPublisher()
+    }
+
+    private func publisherForStep(_ step: CarMakingStep,
+                                  id: Int )
+    -> AnyPublisher<MockDetailOptionEntity, MockDetailRepositoryError>? {
+        switch step {
+        case .powertrain:
+            return detailRepository.fetchPowertrainDetailInfo(id: id)
+        case .driveMethod:
+            return detailRepository.fetchDrivingSystemDetailInfo(id: id)
+        case .bodyType:
+            return detailRepository.fetchBodyTypeDetailInfo(id: id)
+        case .wheelSelection:
+            return detailRepository.fetchWheelDetailInfo(id: id)
+        default:
+            return nil
+        }
+    }
+
+    private func convertToDetailUsecaseError(from error: MockDetailRepositoryError) -> DetailUsecaseError {
+        switch error {
+        case .networkError:
+            return .networkError(error: error)
+        case .conversionError:
+            return .conversionError(error: error)
+        }
+    }
+
+}
