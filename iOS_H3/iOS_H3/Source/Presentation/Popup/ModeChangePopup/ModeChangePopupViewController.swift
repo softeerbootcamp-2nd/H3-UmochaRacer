c4b7eb702f6c7a485d08eeead3eddb983cdea5f1
eigen98 <terrantrace@gmail.com>
2023-08-27 02:56:09 +0900

    feat : ModeChangePopupViewController 구현(#246) - CarMakingMode를 기반으로 현재 선택된 모드를 설정할 수 있도록 초기화 메서드 추가: 이를 통해 외부에서 ModeChangePopupViewController의 인스턴스를 생성할 때, 현재의 차 제작 모드를 직접 설정할 수 있게 되어 뷰 컨트롤러의 재사용성을 향상시킴. - 현재 선택된 모드에 따라 각 버튼의 상태(`inProgress` or `notInProgress`)를 업데이트하는 로직 추가: 사용자에게 현재의 선택 상태를 명확하게 보여주기 위함. - 각 버튼을 클릭했을 때의 액션 구현: 현재 모드와 다른 모드의 버튼을 클릭하면 다른 팝업 뷰를 표시하도록 하며, 같은 모드의 버튼을 클릭하면 팝업을 닫도록 함.

--- iOS_H3/iOS_H3/Source/Presentation/Popup/ModeChangePopup/ModeChangePopupViewController.swift
+++ iOS_H3/iOS_H3/Source/Presentation/Popup/ModeChangePopup/ModeChangePopupViewController.swift
@@ -9,21 +9,76 @@ import UIKit
 
 class ModeChangePopupViewController: UIViewController {
 
+    // MARK: - UI properties
+
+    @IBOutlet weak var popupTitleLabel: UILabel!
+    @IBOutlet weak var selfModeButton: ModeChangeButton!
+
+    @IBOutlet weak var guideModeButton: ModeChangeButton!
+
+    // MARK: - Properties
+
+    var currentMode: CarMakingMode = .selfMode
+    // MARK: - Lifecycles
+
+    init(currentMode: CarMakingMode) {
+        self.currentMode = currentMode
+        super.init(nibName: nil, bundle: nil) // 뷰 컨트롤러가 스토리보드에서 로드되지 않는 경우
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
 
-        // Do any additional setup after loading the view.
     }
 
+    // MARK: - Helpers
+    func setupView() {
+        popupTitleLabel.font = Fonts.mediumTitle2
+        popupTitleLabel.textColor = Colors.mainHyundaiBlue
 
-    /*
-    // MARK: - Navigation
+        selfModeButton.setModeTitle("셀프 모드")
+        selfModeButton.setDescription("가장 일반적으로 많은 사람들이 선택한\n인기있는 옵션들을 보며 내 취향에 맞게 자유롭게\n옵션을 선택할 수 있어요.")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guideModeButton.setModeTitle("가이드 모드")
+        guideModeButton.setDescription("내 상황과 성향에 맞는 옵션들을 추천받아\n나에게 딱 맞는 옵션들로 구성된 차량 견적을\n받아볼 수 있어요.")
+
+        switch currentMode {
+        case .selfMode:
+            selfModeButton.buttonState = .inProgress
+            guideModeButton.buttonState = .notInProgress
+        case .guideMode:
+            selfModeButton.buttonState = .notInProgress
+            guideModeButton.buttonState = .inProgress
+        }
+
+        selfModeButton.addTarget(self, action: #selector(selfModeButtonTapped), for: .touchUpInside)
+        guideModeButton.addTarget(self, action: #selector(guideModeButtonTapped), for: .touchUpInside)
     }
-    */
 
+    @objc func selfModeButtonTapped() {
+        if currentMode != .selfMode {
+            presentOtherPopup()
+        } else {
+            self.dismiss(animated: false)
+        }
+    }
+
+    @objc func guideModeButtonTapped() {
+        if currentMode != .guideMode {
+            presentOtherPopup()
+        } else {
+            self.dismiss(animated: false)
+        }
+    }
+
+    func presentOtherPopup() {
+        self.dismiss(animated: false)
+        // let otherPopupVC = OtherPopupViewController()
+        // self.present(otherPopupVC, animated: true, completion: nil)
+    }
 }
