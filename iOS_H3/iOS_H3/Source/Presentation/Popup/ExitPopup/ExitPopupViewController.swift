21804283220a33f1d74f79d6dccdedc326e6fbcf
eigen98 <terrantrace@gmail.com>
2023-08-27 00:53:43 +0900

    feat: 내차만들기 나가기 팝업 뷰 파일 생성(#246)

---
+++ iOS_H3/iOS_H3/Source/Presentation/Popup/ExitPopup/ExitPopupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExitPopupViewController.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/27.
+//
+
+import UIKit
+
+class ExitPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
