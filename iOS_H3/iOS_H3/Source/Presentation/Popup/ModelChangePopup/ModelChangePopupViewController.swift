97def8bb7d3746f4a2d4c0a187bc89041b520771
eigen98 <terrantrace@gmail.com>
2023-08-27 01:45:20 +0900

    feat : 모델 변경 팝업의 버튼 액션 추가(#246) - 'ModelChangePopupViewController'에서 모델 변경 버튼을 누르면 팝업이 닫히고 이전 화면으로 돌아가는 기능을 구현하였습니다. - 사용자가 "변경할래요!" 버튼을 눌렀을 때, 팝업을 닫고 변경 작업을 위해 이전 화면으로 돌아갈 수 있도록 하기 위함입니다.

--- iOS_H3/iOS_H3/Source/Presentation/Popup/ModelChangePopup/ModelChangePopupViewController.swift
+++ iOS_H3/iOS_H3/Source/Presentation/Popup/ModelChangePopup/ModelChangePopupViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class ModelChangePopupViewController: UIViewController {
+    // MARK: - UI properties
+    @IBOutlet weak var popupTitleLabel: UILabel!
 
+    @IBOutlet weak var popupDescriptionLabel: UILabel!
+
+    @IBOutlet weak var changeButton: OhMyCarSetButton!
+
+    @IBOutlet weak var cancelButton: OhMyCarSetButton!
+
+    // MARK: - Properties
+
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Helpers
+    @IBAction func didTapChangeButton(_ sender: UIButton) {
+        self.dismiss(animated: true) {
+            if let navigationController = self.presentingViewController as? UINavigationController {
+                navigationController.popViewController(animated: true)
+            }
+        }
     }
-    */
+
+    @IBAction func didTapCancelButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ModelChangePopupViewController {
+    func setupView() {
+        changeButton.changeColor(titleColor: Colors.mainHyundaiBlue, backgroundColor: Colors.coolGrey1)
+        changeButton.titleLabel?.font = Fonts.mediumBody2
+        changeButton.setTitle("변경할래요!", for: .normal)
+        cancelButton.titleLabel?.font = Fonts.mediumBody2
+        cancelButton.setTitle("안할래요", for: .normal)
+
+        self.popupTitleLabel.font = Fonts.mediumTitle2
+        self.popupDescriptionLabel.font = Fonts.regularBody2
+
+    }
 
 }
