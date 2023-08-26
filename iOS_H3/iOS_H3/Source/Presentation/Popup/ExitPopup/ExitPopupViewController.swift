0f2b59f0d86b1ea8817dc22b50eebd8584a98cc8
eigen98 <terrantrace@gmail.com>
2023-08-27 01:31:11 +0900

    feat : 팝업의 종료 버튼 기능 추가(#246) - 'ExitPopupViewController'에서 종료 버튼을 누르면 팝업이 닫힌 후 이전 화면으로 돌아가는 기능을 추가하였습니다. - 사용자가 팝업에서 "나갈래요" 버튼을 눌렀을 때, 현재 화면을 닫고 이전 화면으로 돌아갈 수 있도록 하기 위함입니다.

--- iOS_H3/iOS_H3/Source/Presentation/Popup/ExitPopup/ExitPopupViewController.swift
+++ iOS_H3/iOS_H3/Source/Presentation/Popup/ExitPopup/ExitPopupViewController.swift
@@ -9,9 +9,50 @@ import UIKit
 
 class ExitPopupViewController: UIViewController {
 
+    // MARK: - UI properties
+    @IBOutlet weak var popupTitleLabel: UILabel!
+
+    @IBOutlet weak var popupDescriptionLabel: UILabel!
+
+    @IBOutlet weak var exitButton: OhMyCarSetButton!
+
+    @IBOutlet weak var cancelButton: OhMyCarSetButton!
+
+    // MARK: - Properties
+
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupView()
     }
+
+    // MARK: - Helpers
+    @IBAction func didTapExitButton(_ sender: UIButton) {
+        // 팝업 뷰 컨트롤러를 dismiss 하고 나서 popViewController를 호출
+        self.dismiss(animated: true) {
+            if let navigationController = self.presentingViewController as? UINavigationController {
+                navigationController.popViewController(animated: true)
+            }
+        }
+    }
+
+    @IBAction func didTapCancelButton(_ sender: UIButton) {
+        // 팝업 뷰 컨트롤러만 dismiss
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ExitPopupViewController {
+    func setupView() {
+        exitButton.changeColor(titleColor: Colors.mainHyundaiBlue, backgroundColor: Colors.coolGrey1)
+        exitButton.titleLabel?.font = Fonts.mediumBody2
+        exitButton.setTitle("나갈래요", for: .normal)
+        cancelButton.titleLabel?.font = Fonts.mediumBody2
+        cancelButton.setTitle("나가지 않을래요!", for: .normal)
+
+        self.popupTitleLabel.font = Fonts.mediumTitle2
+        self.popupDescriptionLabel.font = Fonts.regularBody2
+
+    }
+
 }
