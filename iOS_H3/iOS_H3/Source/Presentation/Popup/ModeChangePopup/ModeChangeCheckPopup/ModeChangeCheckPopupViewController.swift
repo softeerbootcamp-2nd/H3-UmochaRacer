173cf98be49d139d12b5fb60823e6777389f1f89
eigen98 <terrantrace@gmail.com>
2023-08-27 03:15:30 +0900

    feat : 모드 변경 확인 팝업 뷰 파일 생성(#246)

---
+++ iOS_H3/iOS_H3/Source/Presentation/Popup/ModeChangePopup/ModeChangeCheckPopup/ModeChangeCheckPopupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModeChangeCheckPopupViewController.swift
+//  iOS_H3
+//
+//  Created by KoJeongMin  on 2023/08/27.
+//
+
+import UIKit
+
+class ModeChangeCheckPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
