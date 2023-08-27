f9e55d8582de9d4f1fee0665df5990afd043258a
eigen98 <terrantrace@gmail.com>
2023-08-27 17:33:44 +0900

    fix : DetailError 추가 및 helper함수 분리

--- iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewModel/DetailPopupViewModel.swift
+++ iOS_H3/iOS_H3/Source/Presentation/OptionDetail/ViewModel/DetailPopupViewModel.swift
@@ -8,102 +8,22 @@
 import Combine
 import Foundation
 
-//class DetailPopupViewModel {
-//
-//    // MARK: - Input
-//    struct Input {
-//        var viewDidLoad: PassthroughSubject<Int, Never>
-//        var pageDidChange: CurrentValueSubject<Int, Never>
-//        let selectedURStringRange: PassthroughSubject<NSRange, Never>
-//        var isImageDetail: Bool
-//        let id: Int
-//        let step: CarMakingStep
-//    }
-//
-//    // MARK: - Output
-//    struct Output {
-//        var title = CurrentValueSubject<String, Never>("")
-//        var subTitle = CurrentValueSubject<String, Never>("")
-//        var description = CurrentValueSubject<URString, Never>(URString(fullText: ""))
-//        var additionalInfo = CurrentValueSubject<[URString], Never>([])
-//        var pageCount = CurrentValueSubject<Int, Never>(0)
-//        var imageURL = CurrentValueSubject<String?, Never>(nil)
-//        var selectedURStringRange = PassthroughSubject<NSRange, Never>()
-//    }
-//
-//    // MARK: - Properties
-//    private var cancellables = Set<AnyCancellable>()
-//    private let usecase: DetailPopupUsecaseProtocol
-//
-//    // MARK: - Lifecycles
-//    init(usecase: DetailPopupUsecaseProtocol = DetailPopupUsecase()) {
-//        self.usecase = usecase
-//    }
-//
-//    // MARK: - Helpers
-//    func transform(_ input: Input) -> Output {
-//        let output = Output()
-//
-//        input.viewDidLoad
-//            .flatMap { [weak self] _ -> AnyPublisher<DetailOptionInfo, Never> in
-//                guard let self = self else {
-//                    return Just(MockDetailOptionEntity(title: "",
-//                                                   description: "",
-//                                                   info: nil,
-//                                                   imageSrc: nil).toPresentation())
-//                        .eraseToAnyPublisher()
-//                }
-//                return self.usecase.fetchAllDetails(step: input.step, id: input.id)
-//                    .catch { error -> Just<DetailOptionInfo> in
-//
-//                        return Just(MockDetailOptionEntity(title: "Error",
-//                                                       description: error.localizedDescription,
-//                                                       info: nil,
-//                                                       imageSrc: nil).toPresentation())
-//                    }
-//                    .eraseToAnyPublisher()
-//            }
-//            .receive(on: DispatchQueue.main)
-//            .sink(receiveValue: { detailInfo in
-//                output.pageCount.send(1)
-//                output.title.send(detailInfo.title.fullText)
-//                output.description.send(detailInfo.description)
-//
-//                let infoArray = detailInfo.info.map {
-//                    URString(fullText: "\($0["title"] ?? ""), \($0["description"] ?? "")")
-//                }
-//                output.additionalInfo.send(infoArray)
-//                output.imageURL.send(detailInfo.imageSrc)
-//            })
-//            .store(in: &cancellables)
-//
-//
-//        input.pageDidChange
-//            .flatMap { [weak self] page -> AnyPublisher<DetailOptionInfo, Never> in
-//                guard let self = self else {
-//                    return Just(MockDetailOptionEntity(title: "",
-//                                                   description: "",
-//                                                   info: nil,
-//                                                   imageSrc: nil).toPresentation())
-//                        .eraseToAnyPublisher()
-//                }
-//                return self.usecase.fetchDetailInfo(forPage: page)
-//            }
-//            .sink { detailInfo in
-//                output.title.send(detailInfo.title.fullText)
-//                output.description.send(detailInfo.description)
-//                let infoArray = detailInfo.info.map {
-//                    URString(fullText: "\($0["title"] ?? ""), \($0["description"] ?? "")")
-//                }
-//                output.additionalInfo.send(infoArray)
-//                output.imageURL.send(detailInfo.imageSrc)
-//            }
-//            .store(in: &cancellables)
-//
-//        return output
-//    }
-//}
+enum DetailError: Error {
+    case invalidPageIndex
+    case fetchingFailed
+    case unknown
 
+    var localizedDescription: String {
+        switch self {
+        case .invalidPageIndex:
+            return "잘못된 페이지 접근입니다."
+        case .fetchingFailed:
+            return "자세히보기 정보 조회에 실패하였습니다."
+        case .unknown:
+            return "알 수 없는 에러가 발생하였습니다."
+        }
+    }
+}
 
 class DetailPopupViewModel {
 
@@ -125,11 +45,13 @@ class DetailPopupViewModel {
         var additionalInfo = CurrentValueSubject<[URString], Never>([])
         var pageCount = CurrentValueSubject<Int, Never>(0)
         var imageURL = CurrentValueSubject<String?, Never>(nil)
+        var error = CurrentValueSubject<DetailError?, Never>(nil)
     }
 
     // MARK: - Properties
     private var cancellables = Set<AnyCancellable>()
     private let usecase: DetailPopupUsecaseProtocol
+    private var currentDetails: [DetailOptionInfo] = []
 
     // MARK: - Lifecycles
     init(usecase: DetailPopupUsecaseProtocol = DetailPopupUsecase()) {
@@ -140,73 +62,70 @@ class DetailPopupViewModel {
     func transform(_ input: Input) -> Output {
         let output = Output()
 
-        input.viewDidLoad
-            .flatMap { [weak self] _ -> AnyPublisher<DetailOptionInfo, Never> in
-                guard let self = self else {
-                    return Just(MockDetailOptionEntity(title: "",
-                                                   description: "",
-                                                   info: nil,
-                                                   imageSrc: nil).toPresentation())
-                        .eraseToAnyPublisher()
+        handleViewDidLoad(input, output)
+        handlePageChange(input, output)
+        return output
+    }
+
+    private func handlePageChange(_ input: Input, _ output: Output) {
+        input.pageDidChange
+            .sink { [weak self] pageIndex in
+                guard let self = self else { return }
+                guard pageIndex >= 0 && pageIndex < self.currentDetails.count else {
+                    output.error.send(.invalidPageIndex)
+                    return
                 }
-                return self.usecase.fetchAllDetails(step: input.step, id: input.id)
-                    .catch { error -> Just<DetailOptionInfo> in
-                        return Just(MockDetailOptionEntity(title: "Error",
-                                                       description: error.localizedDescription,
-                                                       info: nil,
-                                                       imageSrc: nil).toPresentation())
-                    }
-                    .eraseToAnyPublisher()
+
+                let detailInfo = self.currentDetails[pageIndex]
+                self.populateOutput(output, with: detailInfo)
+            }
+            .store(in: &cancellables)
+    }
+
+    private func fetchDetails(for input: Input) -> AnyPublisher<[DetailOptionInfo], DetailError> {
+        if input.step == .optionSelection {
+            return usecase.fetchAllAdditionalOptions(id: input.id)
+                .catch { error -> AnyPublisher<[DetailOptionInfo], DetailError> in
+                    return Fail(outputType: [DetailOptionInfo].self, failure: .fetchingFailed).eraseToAnyPublisher()
+                }
+                .eraseToAnyPublisher()
+        } else {
+            return usecase.fetchAllDetails(step: input.step, id: input.id)
+                .map { [$0] }
+                .catch { error -> AnyPublisher<[DetailOptionInfo], DetailError> in
+                    return Fail(outputType: [DetailOptionInfo].self, failure: .fetchingFailed).eraseToAnyPublisher()
+                }
+                .eraseToAnyPublisher()
+        }
+    }
+
+    private func handleViewDidLoad(_ input: Input, _ output: Output) {
+        input.viewDidLoad
+            .flatMap { [weak self] _ -> AnyPublisher<[DetailOptionInfo], DetailError> in
+                self?.fetchDetails(for: input) ?? Fail(outputType: [DetailOptionInfo].self,
+                                                       failure: .unknown).eraseToAnyPublisher()
             }
             .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { detailInfo in
-                output.pageCount.send(1)
-                output.title.send(detailInfo.title.fullText)
-                output.description.send(detailInfo.description)
-
-                let infoArray = detailInfo.info.map {
-                    URString(fullText: "\($0["title"] ?? ""), \($0["description"] ?? "")")
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    output.error.send(error)
                 }
-                output.additionalInfo.send(infoArray)
-                output.imageURL.send(detailInfo.imageSrc)
+            }, receiveValue: { [weak self] details in
+                self?.currentDetails = details
+                output.pageCount.send(details.count)
+                guard let firstDetail = details.first else { return }
+                self?.populateOutput(output, with: firstDetail)
             })
             .store(in: &cancellables)
+    }
 
-        input.pageDidChange
-            .flatMap { [weak self] page -> AnyPublisher<[DetailOptionInfo], Never> in
-                guard let self = self else {
-                    let mockInfo = MockDetailOptionEntity(title: "",
-                                                       description: "",
-                                                       info: nil,
-                                                       imageSrc: nil).toPresentation()
-                    return Just([mockInfo]).eraseToAnyPublisher()
-                }
-                return self.usecase.fetchAllAdditionalOptions(id: input.id)
-                    .catch { error -> Just<[DetailOptionInfo]> in
-                        print("Error: \(error.localizedDescription)")
-                        let mockInfo = MockDetailOptionEntity(title: "Error",
-                                                           description: error.localizedDescription,
-                                                           info: nil,
-                                                           imageSrc: nil).toPresentation()
-                        return Just([mockInfo])
-                    }
-                    .eraseToAnyPublisher()
-            }
-            .sink { detailInfos in
-                guard let firstDetailInfo = detailInfos.first else { return }
-
-                output.title.send(firstDetailInfo.title.fullText)
-                output.description.send(firstDetailInfo.description)
-                let infoArray = firstDetailInfo.info.map {
-                    URString(fullText: "\($0["title"] ?? ""), \($0["description"] ?? "")")
-                }
-                output.additionalInfo.send(infoArray)
-                output.imageURL.send(firstDetailInfo.imageSrc)
-
-                output.pageCount.send(detailInfos.count) // Set the page count
-            }
-            .store(in: &cancellables)
-
-        return output
+    private func populateOutput(_ output: Output, with detail: DetailOptionInfo) {
+        output.title.send(detail.title.fullText)
+        output.description.send(detail.description)
+        let infoArray = detail.info.map {
+            URString(fullText: "\($0["title"] ?? ""), \($0["description"] ?? "")")
+        }
+        output.additionalInfo.send(infoArray)
+        output.imageURL.send(detail.imageSrc)
     }
 }
