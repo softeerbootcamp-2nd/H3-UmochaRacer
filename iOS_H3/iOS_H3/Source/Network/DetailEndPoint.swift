72777151ba8694199475ac7a4ac04c1690041dd2
eigen98 <terrantrace@gmail.com>
2023-08-28 14:56:18 +0900

    fix : 자세히보기 관련 Endpoint 파일 분리(#261)

--- iOS_H3/iOS_H3/Source/Network/DetailEndPoint.swift
+++ iOS_H3/iOS_H3/Source/Network/DetailEndPoint.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+enum DetailEndPoint: Endpoint {
+    case powertrain(id: Int)
+    case drivingSystem(id: Int)
+    case bodyType(id: Int)
+    case wheel(id: Int)
+    case additionalOption(id: Int)
+
+    var baseURL: String { ConstantKey.baseURL }
+    var httpMethod: HTTPMethod { .GET }
+    var headers: [String: String] { [:] }
+
+    var path: String {
+        switch self {
+        case .powertrain(let id):
+            return "detail/powertrain/\(id)"
+        case .drivingSystem(let id):
+            return "detail/driving-system/\(id)"
+        case .bodyType(let id):
+            return "detail/bodytype/\(id)"
+        case .wheel(let id):
+            return "detail/wheel/\(id)"
+        case .additionalOption(let id):
+            return "detail/additional-option/\(id)"
+        }
+    }
+
+    var parameters: HTTPParameter? {
+       return nil
+    }
+}
