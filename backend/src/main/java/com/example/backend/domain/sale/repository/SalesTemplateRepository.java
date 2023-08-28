57d5ef6dfa3c0723aba169517d786db1a5cce89b
Park Seonmi <sm000117@naver.com>
2023-08-28 10:21:56 +0900

    chore: 주석 제거

--- backend/src/main/java/com/example/backend/domain/sale/repository/SalesTemplateRepository.java
+++ backend/src/main/java/com/example/backend/domain/sale/repository/SalesTemplateRepository.java
@@ -37,27 +37,10 @@ public class SalesTemplateRepository {
     }
 
     public List<RatioSummary> findSelectionRatioWithSimilarUsers(String target, EstimateRequest estimateRequest) {
-/*
-        String rowFile = readSQLFile("sql/selectCountWithSimilarUsers.sql");
-        String query = String.format(rowFile, transUriToColumnId(target));
 
-
-        NamedParameterJdbcTemplate namedTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
-
-        MapSqlParameterSource params = new MapSqlParameterSource()
-                .addValue("targetId", transUriToColumnId(target))
-                .addValue("age", estimateRequest.getAge())
-                .addValue("gender", estimateRequest.getGender())
-                .addValue("tag1", estimateRequest.getTag1())
-                .addValue("tag2", estimateRequest.getTag2())
-                .addValue("tag3", estimateRequest.getTag3());
-
-        return namedTemplate.query(query, params, new SelectionRatioRowMapper());
-*/
         String targetId = transUriToColumnId(target);
-//        String query = getQueryDependsOn(targetId)
-//                + getWhereQuery(targetId, estimateRequest);
-        String query = "SELECT "+ targetId + " AS id, COUNT(*) as select_count FROM MODEL m\n" +
+        String query = "SELECT "+ targetId + " AS id, COUNT(*) as select_count " +
+                "FROM MODEL m\n" +
                 "INNER JOIN SALES s ON s.model_id = m.id\n" +
                 "WHERE m.trim_id = 1\n" +
                 "AND(\n" +
@@ -85,16 +68,7 @@ public class SalesTemplateRepository {
         String targetId = transUriToColumnId(target);
         Gender gender = estimateRequest.getGender();
         int age = estimateRequest.getAge();
-/*
-        String query = getWithQuery(targetId) +
-                "select " + targetId + " as id,\n" +
-                "             count(" + targetId + ") as select_count \n" +
-                "      from joined\n" +
-                "      WHERE\n" +
-                "              (" + age + "<=age and age<=" + (age + 9) + " and gender = 'MALE')\n" +
-                "      group by " + targetId + "\n" +
-                "with rollup";
- */
+
         String query = "SELECT s." + targetId + " as id, COUNT(*) as select_count FROM MODEL m \n" +
                 "INNER JOIN SALES s ON s.model_id = m.id \n" +
                 "WHERE " + gender.getQueryString() + " m.trim_id = 1 AND " + age + " <= s.age AND s.age <= " + (age+9) + "\n"+
@@ -104,8 +78,6 @@ public class SalesTemplateRepository {
     }
 
     public List<RatioSummary> findSelectionRatioOfAdditionalOption(EstimateRequest estimateRequest) {
-//        String query = getWithQueryWithAdditionalOption(ADDITIONAL_OPTION_ID)
-//                + getWhereQuery(ADDITIONAL_OPTION_ID, estimateRequest);
         Gender gender = estimateRequest.getGender();
         int age = estimateRequest.getAge();
 
@@ -121,91 +93,5 @@ public class SalesTemplateRepository {
                 "    )\n" +
                 "GROUP BY "+ ADDITIONAL_OPTION_ID + " WITH ROLLUP ";
         return jdbcTemplate.query(query, new SelectionRatioRowMapper());
-    }
-
-    private String getWhereQuery(String targetId, EstimateRequest estimateRequest) {
-        return getWhereQuery(
-                targetId, 
-                estimateRequest.getTag1(),
-                estimateRequest.getTag2(),
-                estimateRequest.getTag3(),
-                estimateRequest.getAge(), 
-                estimateRequest.getGender()
-        );
-    }
-
-    private String getQueryDependsOn(String targetId) {
-        if (targetId.equals("wheel_id")) {
-            return getWithQuery(targetId);
-        }
-        return getWithQueryWhenModelJoined(targetId);
-    }
-
-    private String getWithQueryWhenModelJoined(String targetId) {
-        return "with joined as (select m." + targetId + ", age, gender, tag1, tag2, tag3\n" +
-                "                from SALES s\n" +
-                "                         join MODEL m on s.model_id = m.id\n" +
-                "                where m.trim_id = 1\n" +
-                "                )\n";
-    }
-
-    private String getWithQueryWithAdditionalOption(String targetId) {
-        return "with joined as (select " + targetId + ", age, gender, tag1, tag2, tag3\n" +
-                "          from SALES s \n" +
-                "join SALES_OPTIONS opt on s.id = opt.sales_id" +
-                ")\n";
-    }
-
-    private String getWithQuery(String targetId) {
-        return "with joined as (select " + targetId + ", age, gender, tag1, tag2, tag3\n" +
-                "                from SALES s" +
-                "               )\n";
-    }
-
-    private String getWhereQuery(String targetId, long tag1, long tag2, long tag3, int age, Gender gender) {
-        return "select " + targetId + "       as id,\n" +
-                "       count(" + targetId + ") as select_count\n" +
-                "from joined\n" +
-                "WHERE (\n" +
-                "            (" + tag1 + ", " + tag2 + ", " + tag3 + ") in ((tag1, tag2, tag3),\n" +
-                "                                      (tag1, tag3, tag2),\n" +
-                "                                      (tag2, tag1, tag3),\n" +
-                "                                      (tag2, tag3, tag1),\n" +
-                "                                      (tag3, tag1, tag2),\n" +
-                "                                      (tag3, tag2, tag1)\n" +
-                "            )\n" +
-                "        and (\n" +
-                "                    (" + age + " <= age and age<= " + (age + 9) + " and gender != '" + gender.name() + "')\n" +
-                "                    or ((" + (age - 1) + " <= age or age >= " + (age + 10) + ") and gender = '" + gender.name() + "')\n" +
-                "                    or (" + age + " <= age and age <= " + (age + 9) + " and gender = '" + gender.name() + "')\n" +
-                "                )\n" +
-                "    )\n" +
-                "   or (\n" +
-                "         " + age + " <= age  and age <= " + (age + 9) + "\n" +
-                "        and gender = '" + gender.name() + "'\n" +
-                "        and (\n" +
-                "                            (" + tag1 + ", " + tag2 + ") in ((tag1, tag2), (tag1, tag3),\n" +
-                "                                               (tag2, tag1), (tag2, tag3),\n" +
-                "                                               (tag3, tag1), (tag3, tag2))\n" +
-                "                        or\n" +
-                "                            (" + tag2 + ", " + tag3 + ") in ((tag1, tag2), (tag1, tag3),\n" +
-                "                                               (tag2, tag1), (tag2, tag3),\n" +
-                "                                               (tag3, tag1), (tag3, tag2))\n" +
-                "                        or\n" +
-                "                            (" + tag1 + ", " + tag3 + ") in ((tag1, tag2), (tag1, tag3),\n" +
-                "                                               (tag2, tag1), (tag2, tag3),\n" +
-                "                                               (tag3, tag1), (tag3, tag2))\n" +
-                "                    )\n" +
-                "        and (" + tag1 + ", " + tag2 + ", " + tag3 + ") not in (\n" +
-                "                                          (tag1, tag2, tag3),\n" +
-                "                                          (tag1, tag3, tag2),\n" +
-                "                                          (tag2, tag1, tag3),\n" +
-                "                                          (tag2, tag3, tag1),\n" +
-                "                                          (tag3, tag1, tag2),\n" +
-                "                                          (tag3, tag2, tag1)\n" +
-                "        )\n" +
-                "    )\n" +
-                "group by " + targetId + "\n" +
-                "with rollup";
     }
 }
